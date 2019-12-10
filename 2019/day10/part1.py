5db0394e2487144f58e31d9a96c48524eff781b4
Edwin Torres <CoachEd@gmail.com>
2019-12-10 16:42:52 -0500

    latest

--- 2019/day10/part1.py
+++ 2019/day10/part1.py
@@ -1,9 +1,220 @@
 import sys
 import time
 import math
+from copy import copy, deepcopy
+
+def printArr(a):
+  s = ''
+  for y in range(len(a)):
+    for x in range(len(a[y])):
+      s = s + a[y][x]
+    s = s + '\n'
+  print(s)
+
+def validIndex(x,y,arr):
+  max_y = len(arr) - 1
+  max_x = len(arr[0]) - 1
+  if x < 0 or y < 0:
+    return False
+  if y > max_y or x > max_x:
+    return False
+  return True
 
 print()
 start_secs = time.time()
+
+spacemap =""".#..#
+.....
+#####
+....#
+...##
+"""
+
+# create the original 2D array
+origarr = []
+arr = spacemap.split('\n')[:-1]
+for y in range(len(arr)):
+  temparr = []
+  for x in range(len(arr[y])):
+    temparr.append(arr[y][x])
+  origarr.append(temparr)
+
+# main loop
+for y in range(len(origarr)):
+  for x in range(len(origarr[y])):
+    if origarr[y][x] == '#':
+      # found an steroid at y,x, process it
+      arr2 = deepcopy(origarr)
+      #y,x is me, remove it from the temp arr, so we don't process it later
+      arr2[y][x] = '.'
+
+      # remove all but first one going outwards from me, going UP
+      # me
+      y1 = y
+      x1 = x
+      found = False
+      while(True):
+        y1 = y1 - 1 # go UP
+        if validIndex(x1,y1,arr2):
+          if arr2[y1][x1] == '#':
+            if not found:
+              # we found the first one, leave it
+              found = True
+            else:
+              # remove all others
+              arr2[y1][x1] = '.'
+        else:
+          # went too far
+          break
+
+      # remove all but first one going outwards from me, going DOWN
+      # me
+      y1 = y
+      x1 = x
+      found = False
+      while(True):
+        y1 = y1 + 1 # go DOWN
+        if validIndex(x1,y1,arr2):
+          if arr2[y1][x1] == '#':
+            if not found:
+              # we found the first one, leave it
+              found = True
+            else:
+              # remove all others
+              arr2[y1][x1] = '.'
+        else:
+          # went too far
+          break
+
+      # remove all but first one going outwards from me, going RIGHT
+      # me
+      y1 = y
+      x1 = x
+      found = False
+      while(True):
+        x1 = x1 + 1 # go RIGHT
+        if validIndex(x1,y1,arr2):
+          if arr2[y1][x1] == '#':
+            if not found:
+              # we found the first one, leave it
+              found = True
+            else:
+              # remove all others
+              arr2[y1][x1] = '.'
+        else:
+          # went too far
+          break
+      
+      # remove all but first one going outwards from me, going LEFT
+      # me
+      y1 = y
+      x1 = x
+      found = False
+      while(True):
+        x1 = x1 - 1 # go LEFT
+        if validIndex(x1,y1,arr2):
+          if arr2[y1][x1] == '#':
+            if not found:
+              # we found the first one, leave it
+              found = True
+            else:
+              # remove all others
+              arr2[y1][x1] = '.'
+        else:
+          # went too far
+          break
+
+      # remove all but first one going outwards from me, moving TOP RIGHT
+      # me
+      y1 = y
+      x1 = x
+      found = False
+      while(True):
+        x1 = x1 + 1 # move TOP RIGHT
+        y1 = y1 - 1
+        if validIndex(x1,y1,arr2):
+          if arr2[y1][x1] == '#':
+            if not found:
+              # we found the first one, leave it
+              found = True
+            else:
+              # remove all others
+              arr2[y1][x1] = '.'
+        else:
+          # went too far
+          break
+
+      # remove all but first one going outwards from me, moving BOTTOM RIGHT
+      # me
+      y1 = y
+      x1 = x
+      found = False
+      while(True):
+        x1 = x1 + 1 # move BOTTOM RIGHT
+        y1 = y1 + 1
+        if validIndex(x1,y1,arr2):
+          if arr2[y1][x1] == '#':
+            if not found:
+              # we found the first one, leave it
+              found = True
+            else:
+              # remove all others
+              arr2[y1][x1] = '.'
+        else:
+          # went too far
+          break
+
+      # remove all but first one going outwards from me, moving TOP LEFT
+      # me
+      y1 = y
+      x1 = x
+      found = False
+      while(True):
+        x1 = x1 - 1 # move TOP LEFT
+        y1 = y1 - 1
+        if validIndex(x1,y1,arr2):
+          if arr2[y1][x1] == '#':
+            if not found:
+              # we found the first one, leave it
+              found = True
+            else:
+              # remove all others
+              arr2[y1][x1] = '.'
+        else:
+          # went too far
+          break
+
+      # remove all but first one going outwards from me, moving BOTTOM LEFT
+      # me
+      y1 = y
+      x1 = x
+      found = False
+      while(True):
+        x1 = x1 - 1 # move BOTTOM LEFT
+        y1 = y1 + 1
+        if validIndex(x1,y1,arr2):
+          if arr2[y1][x1] == '#':
+            if not found:
+              # we found the first one, leave it
+              found = True
+            else:
+              # remove all others
+              arr2[y1][x1] = '.'
+        else:
+          # went too far
+          break
+
+      # now loop through remaining #s, get the offset angles, and do the same thing as above
+      # TODO
+      
+
+
+
+
+      
+
+
+
 
 """
 for origin x,y:
