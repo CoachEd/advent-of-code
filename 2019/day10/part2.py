b29b9edadde90ec9182b1e05ba783614dfe0fcca
Edwin Torres <CoachEd@gmail.com>
2019-12-11 08:52:43 -0500

    i hate my answers to this day, but they work

--- 2019/day10/part2.py
+++ 2019/day10/part2.py
@@ -31,11 +31,34 @@ def validIndex(x,y,arr):
 print()
 start_secs = time.time()
 
-spacemap =""".#....#####...#..
-##...##.#####..##
-##...#...#.#####.
-..#.....#...###..
-..#.#.....#....##
+spacemap ="""###..#.##.####.##..###.#.#..
+#..#..###..#.......####.....
+#.###.#.##..###.##..#.###.#.
+..#.##..##...#.#.###.##.####
+.#.##..####...####.###.##...
+##...###.#.##.##..###..#..#.
+.##..###...#....###.....##.#
+#..##...#..#.##..####.....#.
+.#..#.######.#..#..####....#
+#.##.##......#..#..####.##..
+##...#....#.#.##.#..#...##.#
+##.####.###...#.##........##
+......##.....#.###.##.#.#..#
+.###..#####.#..#...#...#.###
+..##.###..##.#.##.#.##......
+......##.#.#....#..##.#.####
+...##..#.#.#.....##.###...##
+.#.#..#.#....##..##.#..#.#..
+...#..###..##.####.#...#..##
+#.#......#.#..##..#...#.#..#
+..#.##.#......#.##...#..#.##
+#.##..#....#...#.##..#..#..#
+#..#.#.#.##..#..#.#.#...##..
+.#...#.........#..#....#.#.#
+..####.#..#..##.####.#.##.##
+.#.######......##..#.#.##.#.
+.#....####....###.#.#.#.####
+....####...##.#.#...#..#.##.
 """
 
 # create the original 2D array
@@ -47,7 +70,7 @@ for y in range(len(arr)):
     temparr.append(arr[y][x])
   origarr.append(temparr)
 
-printArr(origarr)
+#printArr(origarr)
 
 # main loop
 directions = ['up','topright','right','bottomright','down','bottomleft','left','topleft']
@@ -56,10 +79,6 @@ keep_x = -1
 keep_y = -1
 for y in range(len(origarr)):
   for x in range(len(origarr[y])):
-
-    #TEST
-    if not (y == 3 and x == 8):
-      continue
 
     if origarr[y][x] == '#':
       # found an asteroid at y,x, process it
@@ -138,7 +157,7 @@ for y in range(len(origarr)):
               # went too far
               break
 
-      printArr(arr2)
+      #printArr(arr2)
       n = countArr(arr2)
       if (n > max_points):
         max_points = n
@@ -152,19 +171,216 @@ slopes = []
 for y in range(len(origarr)):
   tslopes = []
   for x in range(len(origarr[y])):
-
     denom = keep_x - x
     if denom == 0:
-      m = " {:5.2f} ".format(float(0))
+      slope = 0
     else:
-      m = abs((keep_y -y) / (keep_x - x))
-      m = " {:5.2f} ".format(float(m))
+      slope = abs((keep_y - y) / denom)
+      slope = "{:15.6f} ".format(float(slope))
 
-    tslopes.append(str(m))
-
+    tslopes.append(str(slope))
   slopes.append(tslopes)
-slopes[keep_y][keep_x] = '  **** '
-printArr(slopes)
+slopes[keep_y][keep_x] =  ' **** '
+
+points = []
+
+
+
+
+
+
+origcount = -1
+while not (origcount == countArr(origarr)):
+  origcount = countArr(origarr)
+
+  # one rotation
+
+  # remove UP
+  x = keep_x
+  y = keep_y
+  found = False
+  while not found:
+    y = y - 1
+    if validIndex(x,y,origarr):
+      if origarr[y][x] == '#':
+        found = True
+        points.append([x,y])
+        origarr[y][x] = '.'
+    else:
+      break
+
+  # remove topright quadrant
+  dict1 = {}
+  for y in range(0,keep_y):
+    for x in range(keep_x+1,len(origarr[y])):
+      if origarr[y][x] == '#':
+        if slopes[y][x] in dict1.values():
+          key = list(dict1.keys())[list(dict1.values()).index(slopes[y][x])]
+          tarr = key.split(',')
+          tx = int(tarr[1])
+          ty = int(tarr[0])
+          tdist = abs(keep_x-tx) + abs(keep_y-ty)
+          ndist = abs(keep_x-x) + abs(keep_y-y)
+          if ndist < tdist:
+            # replace
+            del dict1[key]
+            key = str(y) + ',' + str(x)
+            dict1[key] = slopes[y][x]
+        else:
+          key = str(y) + ',' + str(x)
+          dict1[key] = slopes[y][x]
+  #sort dictionary by values
+  dict1 = {k: v for k, v in sorted(dict1.items(), key=lambda item: item[1],reverse=True)}
+  for k,v in dict1.items():
+    tarr = k.split(',')
+    tx = int(tarr[1])
+    ty = int(tarr[0])
+    origarr[ty][tx] = '.'
+    points.append([tx,ty])
+
+  # remove RIGHT
+  x = keep_x
+  y = keep_y
+  found = False
+  while not found:
+    x = x + 1
+    if validIndex(x,y,origarr):
+      if origarr[y][x] == '#':
+        found = True
+        points.append([x,y])
+        origarr[y][x] = '.'
+    else:
+      break
+
+  # remove bottom right quadrant
+  dict1 = {}
+  for y in range(keep_y+1,len(origarr)):
+    for x in range(keep_x+1,len(origarr[y])):
+      if origarr[y][x] == '#':
+        if slopes[y][x] in dict1.values():
+          key = list(dict1.keys())[list(dict1.values()).index(slopes[y][x])]
+          tarr = key.split(',')
+          tx = int(tarr[1])
+          ty = int(tarr[0])
+          tdist = abs(keep_x-tx) + abs(keep_y-ty)
+          ndist = abs(keep_x-x) + abs(keep_y-y)
+          if ndist < tdist:
+            # replace
+            del dict1[key]
+            key = str(y) + ',' + str(x)
+            dict1[key] = slopes[y][x]
+        else:
+          key = str(y) + ',' + str(x)
+          dict1[key] = slopes[y][x]
+  #sort dictionary by values
+  dict1 = {k: v for k, v in sorted(dict1.items(), key=lambda item: item[1])}
+  for k,v in dict1.items():
+    tarr = k.split(',')
+    tx = int(tarr[1])
+    ty = int(tarr[0])
+    origarr[ty][tx] = '.'
+    points.append([tx,ty])
+
+  # remove DOWN
+  x = keep_x
+  y = keep_y
+  found = False
+  while not found:
+    y = y + 1
+    if validIndex(x,y,origarr):
+      if origarr[y][x] == '#':
+        found = True
+        points.append([x,y])
+        origarr[y][x] = '.'
+    else:
+      break
+
+  # remove bottom left quadrant
+  dict1 = {}
+  for y in range(keep_y+1,len(origarr)):
+    for x in range(0,keep_x):
+      if origarr[y][x] == '#':
+        if slopes[y][x] in dict1.values():
+          key = list(dict1.keys())[list(dict1.values()).index(slopes[y][x])]
+          tarr = key.split(',')
+          tx = int(tarr[1])
+          ty = int(tarr[0])
+          tdist = abs(keep_x-tx) + abs(keep_y-ty)
+          ndist = abs(keep_x-x) + abs(keep_y-y)
+          if ndist < tdist:
+            # replace
+            del dict1[key]
+            key = str(y) + ',' + str(x)
+            dict1[key] = slopes[y][x]
+        else:
+          key = str(y) + ',' + str(x)
+          dict1[key] = slopes[y][x]
+  #sort dictionary by values
+  dict1 = {k: v for k, v in sorted(dict1.items(), key=lambda item: item[1],reverse=True)}
+  for k,v in dict1.items():
+    tarr = k.split(',')
+    tx = int(tarr[1])
+    ty = int(tarr[0])
+    origarr[ty][tx] = '.'
+    points.append([tx,ty])  
+
+  # remove LEFT
+  x = keep_x
+  y = keep_y
+  found = False
+  while not found:
+    x = x - 1
+    if validIndex(x,y,origarr):
+      if origarr[y][x] == '#':
+        found = True
+        points.append([x,y])
+        origarr[y][x] = '.'
+    else:
+      break
+
+  # remove top left quadrant
+  dict1 = {}
+  for y in range(0,keep_y):
+    for x in range(0,keep_x):
+      if origarr[y][x] == '#':
+        if slopes[y][x] in dict1.values():
+          key = list(dict1.keys())[list(dict1.values()).index(slopes[y][x])]
+          tarr = key.split(',')
+          tx = int(tarr[1])
+          ty = int(tarr[0])
+          tdist = abs(keep_x-tx) + abs(keep_y-ty)
+          ndist = abs(keep_x-x) + abs(keep_y-y)
+          if ndist < tdist:
+            # replace
+            del dict1[key]
+            key = str(y) + ',' + str(x)
+            dict1[key] = slopes[y][x]
+        else:
+          key = str(y) + ',' + str(x)
+          dict1[key] = slopes[y][x]
+  #sort dictionary by values
+  dict1 = {k: v for k, v in sorted(dict1.items(), key=lambda item: item[1])}
+  for k,v in dict1.items():
+    tarr = k.split(',')
+    tx = int(tarr[1])
+    ty = int(tarr[0])
+    origarr[ty][tx] = '.'
+    points.append([tx,ty])  
+
+
+
+"""
+what do you get if you multiply its (200th asteroid) X coordinate by 100 and then add its Y coordinate? 
+"""
+asteroid200 = points[199]
+answer = int(asteroid200[0]) * 100 + (asteroid200[1])
+print(answer)
+#num = 1
+#for p in points:
+#  print(str(num) + ' x,y: ' + str(p[0]) + ',' + str(p[1]))
+#  num = num + 1
+
+
 
 end_secs = time.time()
 print('elapsed time: ' + str(end_secs - start_secs) + ' seconds')
