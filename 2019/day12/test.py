1a007749e9cc4b13f6c964d6e713c50a76164915
Edwin Torres <CoachEd@gmail.com>
2020-01-03 14:48:52 -0500

    latest

--- 2019/day12/test.py
+++ 2019/day12/test.py
@@ -11,6 +11,8 @@ inp1 = """<x=3, y=2, z=-6>
 <x=-8, y=-1, z=13>
 <x=5, y=10, z=4>
 """
+
+
 inp1 = inp1.replace("<x=", "")
 inp1 = inp1.replace("y=", "")
 inp1 = inp1.replace("z=", "")
@@ -28,11 +30,6 @@ for r in tarr:
 
   posarr.append(temp + temp2)
 
-
-
-
-
-
 """
 To apply gravity, consider every pair of moons. 
 On each axis (x, y, and z), the velocity of each moon changes by exactly +1 or -1 to pull the moons together. 
@@ -40,6 +37,17 @@ For example, if Ganymede has an x position of 3, and Callisto has a x position o
 then Ganymede's x velocity changes by +1 (because 5 > 3) and Callisto's x velocity changes by -1 (because 3 < 5). 
 However, if the positions on a given axis are the same, the velocity on that axis does not change for that pair of moons.
 """
+
+gravs = {}
+gravs['abcd'] = [3,1,-1,-3]
+gravs['aaaa'] = [0,0,0,0]
+gravs['aacd'] = [2,2,-1,-3]
+gravs['aaad'] = [1,1,1,-3]
+gravs['abbd'] = [3,0,0,-3]
+gravs['abbb'] = [3,-1,-1,-1]
+gravs['abcc'] = [3,1,-2,-2]
+gravs['aacc'] = [2,2,-2,-2]
+
 steps = 1000
 for t in range(1,steps+1):
   # steps 1 to steps
@@ -49,21 +57,33 @@ for t in range(1,steps+1):
   # 3, 1, -1, -3
 
   # apply gravity
-  for i in range(4):
-    posarr.sort(key = lambda x: x[2])
-    for p in range(4):
-      posarr[p][i+3] = posarr[p][i+3] + gravs[i]
+  for i in range(3):
+    posarr.sort(key = lambda x: x[i])
+    order_arr = ['a','b','c','d'] 
+    if posarr[0][i] == posarr[1][i]:
+      order_arr[1] = order_arr[0]
+    if posarr[1][i] == posarr[2][i]:
+      order_arr[2] = order_arr[1]
+    if posarr[2][i] == posarr[3][i]:
+      order_arr[3] = order_arr[2]
+    order_str = ''.join(order_arr)
+    plus_arr = gravs[order_str]
+    #print(posarr)
+    #print(plus_arr)
+    for h in range(4):
+       posarr[h][i+3]  = posarr[h][i+3] + plus_arr[h]
+    #print(posarr)
+    #print()s
 
   # move planets
   for p in posarr:
     for i in range(3):
       p[i] = p[i] + p[i+3]
-
-
-
-
+  #print(posarr)
+  
 
 totenergy = 0
+
 for p in posarr:
   kinenergy = 0
   potenergy = 0
