979f1d1b504cad65f3608eac7733f70970d55b48
Ed Torres <ertorres@mitre.org>
2020-12-01 10:35:51 -0500

    latest

--- 2020/day01/part1.py
+++ 2020/day01/part1.py
@@ -1,4 +1,8 @@
 import sys
+import time
+
+start_secs = time.time()
+
 l=[]
 my_file = open("inp.txt", "r")
 Lines = my_file.readlines()
@@ -12,6 +16,8 @@ for i in range(0,len(l)-1):
     for j in range (i+1,len(l)):
         if (l[i]+l[j]) == 2020:
             print('Part 1: '+str(l[i]*l[j]))
+            end_secs = time.time()
+            print(str(end_secs-start_secs))             
             sys.exit()
         elif (l[i]+l[j]) > 2020:
             break
--- 2020/day01/part2.py
+++ 2020/day01/part2.py
@@ -1,5 +1,9 @@
 # Part 2
 import sys
+import time
+
+start_secs = time.time()
+
 l=[]
 my_file = open("inp.txt", "r")
 Lines = my_file.readlines()
@@ -13,6 +17,9 @@ for i in range(0,len(l)-2):
         for k in range(j+1,len(l)):
             if ( l[i] + l[j] + l[k] ) == 2020:
                 print('Part 2: '+str(l[i] *l[j] * l[k]))
+                end_secs = time.time()
+                print(str(end_secs-start_secs))                
                 sys.exit()
             elif ( l[i] + l[j] + l[k] ) > 2020:
                 break
+
