f15394643aa9cb23361ea7f4891d1a08a5d39903
Ed Torres <CoachEd@gmail.com>
2020-12-06 11:06:15 -0500

    latest

--- 2020/day06/part1.py
+++ 2020/day06/part1.py
@@ -1,19 +1,24 @@
 import sys
-import time
-
-start_secs = time.time()
-
-# read in input file
 l=[]
+i=0
 my_file = open("inp.txt", "r")
-lines = my_file.readlines()
-for line in lines: 
-    l.append(line.strip())
+Lines = my_file.readlines()
+tmp=''
+for line in Lines:
+    str1 = line.strip()
+    if len(str1) == 0:
+        l.append(tmp)
+        tmp = ''
+    else:
+        tmp = tmp + str1
 
-# TODO
-
-
-
-
-end_secs = time.time()
-print(str(end_secs-start_secs))
+cnt = 0
+for s in l:
+    while len(s) > 0:
+        c = s[0]
+        cnt = cnt + 1
+        s = s.replace(c,'')
+print(cnt)
+    
+        
+       
--- 2020/day06/part2.py
+++ 2020/day06/part2.py
@@ -1,22 +1,32 @@
 import sys
-import time
-
-start_secs = time.time()
-
-# read in input file
 l=[]
+i=0
 my_file = open("inp.txt", "r")
-lines = my_file.readlines()
-for line in lines: 
-    l.append(line.strip())
+Lines = my_file.readlines()
+tmp=''
+groups=0
+for line in Lines:
+    str1 = line.strip()
+    if len(str1) == 0:
+        l.append(tmp+':'+str(groups))
+        tmp = ''
+        groups=0
+    else:
+        tmp = tmp + str1
+        groups=groups+1
 
-# TODO
-
-
-
-
-
-
-
-end_secs = time.time()
-print(str(end_secs-start_secs))
+cnt = 0
+for s in l:
+    arr=s.split(':')
+    s = arr[0]
+    groups=arr[1]
+    while len(s) > 0:
+        c = s[0]
+        if s.count(c) == int(groups):
+            cnt = cnt + 1
+        s = s.replace(c,'')
+        
+print(cnt)
+    
+        
+       
