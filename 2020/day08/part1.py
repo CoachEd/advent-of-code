9df3cad5af6bb37e054571051215235f458447ae
Ed Torres <ertorres@mitre.org>
2020-12-09 08:49:37 -0500

    latest

--- 2020/day08/part1.py
+++ 2020/day08/part1.py
@@ -1,19 +1,45 @@
 import sys
-import time
 
-start_secs = time.time()
-
-# read in input file
 l=[]
+
 my_file = open("inp.txt", "r")
-lines = my_file.readlines()
-for line in lines: 
+Lines = my_file.readlines()
+for line in Lines:
     l.append(line.strip())
 
-# TODO
+rst = '\x1b[0m' # reset to default colors
+fg = '\x1b[30m'
+bg = '\x1b[103m'
+def print_instr(curr_i):
 
+    for i in range(0,len(l)):
+        if i % 19 == 0 and i != 0:
+            print()    
+        if i == curr_i:
+            sys.stdout.write(fg+bg+l[i].ljust(8)+rst + ' ')
+        else:
+            sys.stdout.write(l[i].ljust(8) + ' ')
+    sys.stdout.write('\033[0;0H')
+    print()
 
-
-
-end_secs = time.time()
-print(str(end_secs-start_secs))
+accumulator=0
+new_instr = True
+i=0
+seen=len(l) * [0]
+while new_instr:
+    if seen[i] > 0:
+        break
+    seen[i] = seen[i]+1
+    s=l[i]
+    #print_instr(i)
+    arr = s.split(' ')
+    cmd=arr[0]
+    arg=int(arr[1])
+    if cmd=='acc':
+        accumulator = accumulator + arg
+        i = i + 1
+    elif cmd == 'jmp':
+        i = i + arg
+    elif cmd == 'nop':
+        i  = i + 1
+print('Part 1: ' + str(accumulator))
--- 2020/day08/part2.py
+++ 2020/day08/part2.py
@@ -1,22 +1,85 @@
 import sys
-import time
 
-start_secs = time.time()
+rst = '\x1b[0m' # reset to default colors
+fgb = '\x1b[30m'
+fgw = '\x1b[37m'
+bgy = '\x1b[103m'
+bgg = '\x1b[102m'
+bgw = '\x1b[47m'
+bgb = '\x1b[104m'
+def print_instr(curr_i):
+    print()
+    for i in range(0,len(l)):
+        b = ''
+        f = ''
+        if i % 19 == 0 and i != 0:
+            print()    
+        if l[i].find('jmp') != -1:
+            b = bgg
+            f = fgb
+        elif l[i].find('acc') != -1:
+            b = bgb
+            f = fgw
+        else:
+            b = bgw
+            f = fgb
 
-# read in input file
+        if i == curr_i:
+            sys.stdout.write(f+b+l[i].ljust(8)+' '+rst)
+        else:
+            sys.stdout.write(l[i].ljust(8) + ' ')
+    sys.stdout.write('\033[0;0H')
+    print()
+
 l=[]
+
 my_file = open("inp.txt", "r")
-lines = my_file.readlines()
-for line in lines: 
+Lines = my_file.readlines()
+for line in Lines:
     l.append(line.strip())
+    
+accumulator=0
+l0 = l.copy()
 
-# TODO
+def run_program():
+    global accumulator
+    i=0
+    seen=len(l) * [0]
+    while i < len(l):
+        if seen[i] > 0:
+            return False
+        seen[i] = seen[i]+1
+        s=l[i]
+        arr = s.split(' ')
+        cmd=arr[0]
+        arg=int(arr[1])
+        print_instr(i)
+        if cmd=='acc':
+            accumulator = accumulator + arg
+            i = i + 1
+        elif cmd == 'jmp':
+            i = i + arg
+        elif cmd == 'nop':
+            i  = i + 1
+            
+    return True
 
+for i in range(0, len(l0)):
+    l = l0.copy()
+    s = l[i]
+    accumulator = 0
+    arr = s.split(' ')
+    cmd=arr[0]
+    if cmd == 'acc':
+        continue
+    if cmd == 'jmp':
+        cmd ='nop'
+    elif cmd == 'nop':
+        cmd = 'jmp'
+    
+    l[i] = cmd + ' ' + arr[1]
+    done = run_program()
+    if done:
+        break
 
-
-
-
-
-
-end_secs = time.time()
-print(str(end_secs-start_secs))
+print(accumulator)
