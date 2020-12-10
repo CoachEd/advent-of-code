ebea5084491ad319be80af993331b3e080a0d7ae
Ed Torres <ertorres@mitre.org>
2020-12-10 11:52:53 -0500

    latest

--- 2020/day10/part2.py
+++ 2020/day10/part2.py
@@ -2,44 +2,6 @@ import sys
 import time
 
 start_secs = time.time()
-
-def build_tree(t,l,i,src):
-    if len(l) == 0:
-        t[src] = [-2] #leaf
-        return
-    t[src] = [-1,-1,-1]
-    #print(l[0])
-    if i >= len(l):
-        t[src] = [-2]
-        return
-    if (l[i] - src) == 1:
-        n = l[i]
-        i = i + 1
-        t[src][0] = n
-        build_tree(t,l,i,n)
-    if i < len(l) and (l[i] - src) == 2:
-        n = l[i]
-        i = i + 1
-        t[src][1] = n
-        build_tree(t,l,i,n)
-    if i < len(l) and (l[i] - src) == 3:
-        n = l[i]
-        i = i + 1
-        t[src][2] = n
-        build_tree(t,l,i,n)
-    return t
-
-def count_leaves(t,i):
-    if t[i][0] == -2:
-        return 1
-    cnt = 0
-    if t[i][0] != -1:
-        cnt = cnt + count_leaves(t,t[i][0])
-    if t[i][1] != -1:
-        cnt = cnt + count_leaves(t,t[i][1])
-    if t[i][2] != -1:
-        cnt = cnt + count_leaves(t,t[i][2])
-    return cnt
  
 l=[]
 my_file = open("inp.txt", "r")
@@ -48,12 +10,47 @@ for line in Lines:
     l.append(int(line.strip()))
 
 l.sort()
-t=dict()
-print('building tree...')
-build_tree(t,l,0,0)
+max = l[-1]
+#print(l)
 
-print('counting leaves...')
-print( count_leaves(t,0) )
+d = dict()
+dcounts = dict()
+for i in range(0,len(l)):
+    d[l[i]] = 1
+
+def count_paths(src):
+
+    if src == max:
+        return 1
+    num_paths = 0
+    left = src+1
+    mid = src+2
+    right = src+3
+
+    if left in d:
+        if left in dcounts:
+            num_paths = num_paths + dcounts[left]
+        else:
+            x = count_paths(left)
+            dcounts[left] = x
+            num_paths = num_paths + x
+    if mid in d:
+        if mid in dcounts:
+            num_paths =  num_paths + dcounts[mid]
+        else:
+            x = count_paths(mid)
+            dcounts[mid] = x
+            num_paths = num_paths + x
+    if right in d:
+        if right in dcounts:
+            num_paths = num_paths + dcounts[right]
+        else:
+            x = count_paths(right)
+            dcounts[right] = x
+            num_paths = num_paths + x
+    return num_paths
+
+print(count_paths(0))
 
 end_secs = time.time()
 print()
