2a03b5cc2bf88196359968ba9389d7e9a601a546
Ed Torres <ertorres@mitre.org>
2020-12-11 09:26:28 -0500

    latest

--- 2020/day11/part1.py
+++ 2020/day11/part1.py
@@ -1,19 +1,127 @@
 import sys
+from copy import copy, deepcopy
 import time
 
 start_secs = time.time()
 
-# read in input file
+rst = '\x1b[0m' # reset to defaults
+fgg = '\x1b[92m'
+hm = '\033[1;1H' # x,y 1,1
+def print_seats(l,chg):
+
+    #sys.stdout.write('\033[0;0H') # reset cursor pos
+    s = '\n'+hm+'\n\n'
+    for r in range(0,len(l)):
+        for c in range(0,len(l[r])):
+            if chg[r][c] == 1:
+                s = s + fgg + l[r][c]+ rst
+            else:
+                s = s + l[r][c]
+        s = s + '\n'
+    sys.stdout.write(s+rst)
+    time.sleep(0.10)
+
+
+
+def v(l,r,c):
+    b1 = r >= 0 and r < len(l)
+    b2 = c >= 0 and c < len(l[0])
+    if (b1 and b2):
+        return l[r][c]
+    else:
+        return ''
+
+def seats(l,r,c):
+    tr=r-1
+    tc=c
+    br=r+1
+    bc=c
+    rr=r
+    rc=c+1
+    lr=r
+    lc=c-1
+    trr=r-1
+    trc=c+1
+    brr=r+1
+    brc=c+1
+    tlr=r-1
+    tlc=c-1
+    blr=r+1
+    blc=c-1
+
+    n  = ''
+    n  = n + v(l,tr,tc)
+    n  = n + v(l,br,bc)
+    n  = n + v(l,rr,rc)
+    n  = n + v(l,lr,lc)
+    n  = n + v(l,trr,trc)
+    n  = n + v(l,brr,brc)
+    n  = n + v(l,tlr,tlc)
+    n  = n + v(l,blr,blc)
+    return n
+
 l=[]
+
 my_file = open("inp.txt", "r")
-lines = my_file.readlines()
-for line in lines: 
+Lines = my_file.readlines()
+for line in Lines:
     l.append(line.strip())
 
-# TODO
+rows = len(l)
+cols = len(l[0])
+chg = [ [None]*cols for i in range(rows)]
+a = [ [None]*cols for i in range(rows)]
+for r in range(0,len(a)):
+    for c in range(0,len(a[r])):
+        a[r][c] = l[r][c]
+        chg[r][c] = 0
 
+phs=0
+print_seats(a,chg)
+while True:        	
+    #print(phs)
+    z = deepcopy(a)
+    chg2=deepcopy(chg)
+    changes = False
+    for r in range(0,len(a)):
+        for c in range(0,len(a[r])):
+            s = seats(a,r,c)
+            occ = s.count('#')
+            emp = s.count('L')
+            if a[r][c] == 'L' and occ==0:
+                changes = True
+                z[r][c] = '#'
+                chg2[r][c] = 1
+            elif a[r][c] == '#' and occ >= 4:
+                changes = True
+                z[r][c] = 'L'
+                chg2[r][c] = 1
+    phs = phs + 1
+    a = z
+    print_seats(a,chg2)
+    if not changes:
+        break
 
-
-
+occ = 0
+for r in range(0,len(a)):
+    for c in range(0,len(a[r])):
+        if a[r][c] == '#':
+            occ = occ +1
+print('part 1: ' + str(occ))
+       
 end_secs = time.time()
-print(str(end_secs-start_secs))
+print()
+print(str(end_secs-start_secs))    
+#If a seat is empty (L) and there are 
+#no occupied seats adjacent to it, 
+#the seat becomes occupied.
+
+#If a seat is occupied (#) and four 
+#or more seats adjacent to it are 
+#also occupied, the seat becomes empty.
+
+#Otherwise, the seat's state does 
+#not change.
+
+
+
--- 2020/day11/part2.py
+++ 2020/day11/part2.py
@@ -1,22 +1,126 @@
 import sys
+from copy import copy, deepcopy
 import time
 
 start_secs = time.time()
 
-# read in input file
+def v(l,r,c):
+    b1 = r >= 0 and r < len(l)
+    b2 = c >= 0 and c < len(l[0])
+    if (b1 and b2):
+        return l[r][c]
+    else:
+        return 'X'
+
+def seats(l,r,c):
+    
+    i=1
+    s1=''
+    s2=''
+    s3=''
+    s4=''
+    s5=''
+    s6=''
+    s7=''
+    s8=''
+    n=''
+    while True:
+        tr=r-i
+        tc=c
+        br=r+i
+        bc=c
+        rr=r
+        rc=c+i
+        lr=r
+        lc=c-i
+        trr=r-i
+        trc=c+i
+        brr=r+i
+        brc=c+i
+        tlr=r-i
+        tlc=c-i
+        blr=r+i
+        blc=c-i
+
+        if s1 == '' or s1 == '.':
+            s1 = v(l,tr,tc)
+        if s2 == '' or s2 == '.':
+            s2 = v(l,br,bc)
+        if s3 == '' or s3 == '.':
+            s3= v(l,rr,rc)
+        if s4 == '' or s4 == '.':
+            s4 = v(l,lr,lc)
+        if s5 == '' or s5 == '.':
+            s5=v(l,trr,trc)
+        if s6 == '' or s6 == '.':
+            s6=v(l,brr,brc)
+        if s7 == '' or s7 == '.':
+            s7=v(l,tlr,tlc)
+        if s8 == '' or s8 == '.':
+            s8=v(l,blr,blc)
+        i=i+1
+        n=s1+s2+s3+s4+s5+s6+s7+s8
+        if n.count('.') == 0:
+            break
+    #1966
+    return n
+
 l=[]
+
 my_file = open("inp.txt", "r")
-lines = my_file.readlines()
-for line in lines: 
+Lines = my_file.readlines()
+for line in Lines:
     l.append(line.strip())
 
-# TODO
+rows = len(l)
+cols = len(l[0])
 
+a = [ [None]*cols for i in range(rows)]
+for r in range(0,len(a)):
+    for c in range(0,len(a[r])):
+        	a[r][c] = l[r][c]
 
+phs=0
+while True:        	
+    #print(phs)
+    z = deepcopy(a)
+    changes = False
+    for r in range(0,len(a)):
+        for c in range(0,len(a[r])):
+            s = seats(a,r,c)
+            occ = s.count('#')
+            emp = s.count('L')
+            if a[r][c] == 'L' and occ==0:
+                changes = True
+                z[r][c] = '#'
+            elif a[r][c] == '#' and occ >= 5:
+                changes = True
+                z[r][c] = 'L'
+    phs = phs + 1
+    a = z
+    if not changes:
+        break
 
-
-
-
-
+occ = 0
+for r in range(0,len(a)):
+    for c in range(0,len(a[r])):
+        if a[r][c] == '#':
+            occ = occ +1
+print('part 2: ' + str(occ))
 end_secs = time.time()
-print(str(end_secs-start_secs))
+print()
+print(str(end_secs-start_secs))           
+
+#If a seat is empty (L) and there are 
+#no occupied seats adjacent to it, 
+#the seat becomes occupied.
+
+#If a seat is occupied (#) and five
+#or more seats adjacent to it are 
+#also occupied, the seat becomes empty.
+
+#Otherwise, the seat's state does 
+#not change.
+
+
+
