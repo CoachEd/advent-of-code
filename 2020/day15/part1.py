236c6baaaf887e8b83db418572b9d827184e3049
Ed Torres <CoachEd@gmail.com>
2020-12-15 07:37:24 -0500

    latest

--- 2020/day15/part1.py
+++ 2020/day15/part1.py
@@ -8,9 +8,16 @@ import functools
 
 start_secs = time.time()
 
-n = [19,0,5,1,10,13]
-n=[0,3,6]
+"""
+Given the starting numbers 1,3,2, the 2020th number spoken is 1.
+Given the starting numbers 2,1,3, the 2020th number spoken is 10.
+Given the starting numbers 1,2,3, the 2020th number spoken is 27.
+Given the starting numbers 2,3,1, the 2020th number spoken is 78.
+Given the starting numbers 3,2,1, the 2020th number spoken is 438.
+Given the starting numbers 3,1,2, the 2020th number spoken is 1836.
+"""
 
+n = [19,0,5,1,10,13]
 
 size=2020+1
 nums = n + [None]*size
@@ -24,8 +31,7 @@ prev=len(n)-1
 while True:
     arr = d[nums[prev]]
     if len(arr) > 1:
-        lturn=arr[-1]
-        newnum=turn-lturn
+        newnum=arr[-1]-arr[-2]
         nums[prev+1]=newnum
         if not newnum in d:
             d[newnum]=[turn]
@@ -33,17 +39,17 @@ while True:
             d[newnum].append(turn)
     else:
         nums[prev+1] = 0
-        d[0].append(turn)
-        
-    if turn  == 10:
+        if not 0 in d:
+            d[0] = [turn]
+        else:
+            d[0].append(turn)
+    if turn  == 2020:
         break
     turn = turn + 1
     prev= prev+1
-print(nums)
+
 print('part 1: '+str(nums[turn-1]))
     
-
-
 end_secs = time.time()
 print()
 print(str(end_secs-start_secs) + ' seconds')
--- 2020/day15/part2.py
+++ 2020/day15/part2.py
@@ -1,22 +1,59 @@
+
+
 import sys
 import time
+import math
+import random
+import functools
 
 start_secs = time.time()
 
-# read in input file
-l=[]
-my_file = open("inp.txt", "r")
-lines = my_file.readlines()
-for line in lines: 
-    l.append(line.strip())
+"""
+Given the starting numbers 1,3,2, the 2020th number spoken is 1.
+Given the starting numbers 2,1,3, the 2020th number spoken is 10.
+Given the starting numbers 1,2,3, the 2020th number spoken is 27.
+Given the starting numbers 2,3,1, the 2020th number spoken is 78.
+Given the starting numbers 3,2,1, the 2020th number spoken is 438.
+Given the starting numbers 3,1,2, the 2020th number spoken is 1836.
+"""
 
-# TODO
+n = [19,0,5,1,10,13]
 
+size=30000000+1
+nums = n + [None]*size
+d=dict()
+turn = 1
+for e in n:
+    d[e]=[turn]
+    turn = turn + 1
+    
+prev=len(n)-1
+while True:
+    if turn % 1000000 == 0:
+        print(turn)
+    arr = d[nums[prev]]
+    if len(arr) > 1:
+        newnum=arr[-1]-arr[-2]
+        nums[prev+1]=newnum
+        if not newnum in d:
+            d[newnum]=[turn]
+        else:
+            d[newnum].append(turn)
+    else:
+        nums[prev+1] = 0
+        if not 0 in d:
+            d[0] = [turn]
+        else:
+            d[0].append(turn)
+    if turn  == 30000000:
+        break
+    turn = turn + 1
+    prev= prev+1
 
-
-
-
-
-
+print('part 1: '+str(nums[turn-1]))
+    
 end_secs = time.time()
-print(str(end_secs-start_secs))
+print()
+print(str(end_secs-start_secs) + ' seconds')
+
+
