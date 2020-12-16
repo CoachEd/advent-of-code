ac410235ebe0be1b94048ca9069f7b0984a201e4
Ed Torres <CoachEd@gmail.com>
2020-12-16 08:37:04 -0500

    latest

--- 2020/day13/part2.py
+++ 2020/day13/part2.py
@@ -1,3 +1,4 @@
+
 import sys
 import time
 import math
@@ -22,6 +23,56 @@ def get_times(l,t,d):
 
 # INPUT DATA
 s='23,x,x,x,x,x,x,x,x,x,x,x,x,41,x,x,x,x,x,x,x,x,x,509,x,x,x,x,x,x,x,x,x,x,x,x,13,17,x,x,x,x,x,x,x,x,x,x,x,x,x,x,29,x,401,x,x,x,x,x,37,x,x,x,x,x,x,x,x,x,x,x,x,19'
+s='23,x,x,x,x,x,x,x,x,x,x,x,x,41'
+t=0
+every=1
+# 69,943
+
+s='41,x,x,x,x,x,x,x,x,x,509'
+t= 69+13
+every=943
+
+
+s='509,x,x,x,x,x,x,x,x,x,x,x,x,13'
+t=398028 + 10
+every=479987
+
+s='13,17'
+t=4717921+ 13
+every=6239831
+
+
+s='17,x,x,x,x,x,x,x,x,x,x,x,x,x,x,29'
+t= 35917089 + 1
+every= 106077127
+
+
+s='29,x,401'
+t= 2687845265+15
+every= 3076236683
+
+
+s='401,x,x,x,x,x,37'
+t= 107279892502+2
+every= 1233570909883
+
+
+s='37,x,x,x,x,x,x,x,x,x,x,x,x,19'
+t= 43282261738409+6
+every= 45642123665671
+
+#ans: 
+#     225850756401099
+#evr: 867200349647749
+
+
+
+s='23,x,x,x,x,x,x,x,x,x,x,x,x,41,x,x,x,x,x,x,x,x,x,509,x,x,x,x,x,x,x,x,x,x,x,x,13,17,x,x,x,x,x,x,x,x,x,x,x,x,x,x,29,x,401,x,x,x,x,x,37,x,x,x,x,x,x,x,x,x,x,x,x,19'
+t=225850756401099
+every=1
+
+
+
 
 #s='23,x,x,x,x,x,x,x,x,x,x,x,x,41' # 69, every 943
 #s='41,x,x,x,x,x,x,x,x,x,509'  # 398028, every 479987
@@ -38,22 +89,33 @@ s='23,x,x,x,x,x,x,x,x,x,x,x,x,41,x,x,x,x,x,x,x,x,x,509,x,x,x,x,x,x,x,x,x,x,x,x,1
 
 
 
+#s='7,13,x,x,59,x,31,19'
+#s='7,13' # ans,evr 77,91
+#s='13,x,x,59'
+#s='59,x,31'
+#s='31,19'
+#t= 70151+2 # prev+spots
+#every= 166439
 
-#every = 1 #default
-#s='7,13,x,x,59,x,31,19' 
+#real answer 1068788
+#ans:        1068787
 
 
 
+#[QPython] Press enter to exit ...
 
 
 #s='4,x,x,5,6' # 32 , then every 60
 #s='4,x,x,5' # 12 , then every 20
 #s='5,6' # 35 - 3 (spots back) = 32
-#t=15 # 12+3
+#t=12+3    # 12+3
 #every = 20
 
-
-
+# sample tests
+#s='67,7,59,61' # first occurs at timestamp 754018.
+#s='67,x,7,59,61' # first occurs at timestamp 779210.
+#s='67,7,x,59,61' # first occurs at timestamp 1261476.
+#s='1789,37,47,1889' # first occurs at timestamp 1202161486.
 
 arr = s.split(',')
 times=[]
@@ -69,17 +131,44 @@ found = False
 
 print(d)
 
+"""
+s='67,7,59,61' # first occurs at timestamp 754018.
+s='67,x,7,59,61' # first occurs at timestamp 779210.
+s='67,7,x,59,61' # first occurs at timestamp 1261476.
+s='1789,37,47,1889' # first occurs at timestamp 1202161486.
+"""
+#print(get_times(times,754018,d))
+#print(get_times(times,779210,d))
+#print(get_times(times,1261476,d))
+#print(get_times(times,1202161486,d))
+#sys.exit()
+
+# ATTEMPT
+print(get_times(times,225850756401099,d))
+sys.exit()
+
+"""
+t=225850756401099
+for i in range(t-200,t+200):
+    b = get_times(times,i,d)
+    if b:
+        print(i)
+        break
+sys.exit()
+"""
+
+
 
 print()
 print('Running...')
-t=225850756400993
-for i in range(t,999999999999999,1):
-    b = get_times(times,i,d)
-    if b:
-        print('FOUND: ' + str(i))
-        break
-        
-sys.exit()
+#t=225850756400993
+#for i in range(t,999999999999999,1):
+#    b = get_times(times,i,d)
+#    if b:
+#        print('FOUND: ' + str(i))
+#        break
+       
+#sys.exit()
 #every = 23
 #n=1000000
 #for i in range(225850756400740-n,225850756400740+n,1):
@@ -89,13 +178,6 @@ sys.exit()
 
 #print('done')
 #sys.exit()
-
-
-
-
-
-
-
 
 
 iterations = 0
@@ -120,12 +202,14 @@ while True:
         # TESTING
         if False:
             reps = reps + 1
-            print(t)
+            arr4.append(t)
             if (reps < 3):
                 t = t + every
                 continue
-            print()
+            print('ans: '+str(arr4[0]))
+            print('evr: '+str(arr4[2]-arr4[1]))
 
+        print()
         print('Part 2: ' + str(t))
         break
     t = t + every
@@ -133,3 +217,5 @@ while True:
 end_secs = time.time()
 print()
 print(str(end_secs-start_secs) + ' seconds')
+
+
