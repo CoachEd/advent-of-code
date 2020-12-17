5312bb1a2f24af7960258196725b9e2fa6f70c19
Ed Torres <CoachEd@gmail.com>
2020-12-16 21:58:06 -0500

    latest

--- 2020/day16/part2.py
+++ 2020/day16/part2.py
@@ -74,16 +74,15 @@ for i in range(0,len(ranges)):
             minf = j
         if j > maxf:
             maxf = j 
-        v[j]=1
+        v[j]=v[j]+1
         vfields[j].add(field)
     for j in range(x1,y1+1):
         if j < minf:
             minf = j
         if j > maxf:
             maxf = j         
-        v[j]=1
+        v[j]=v[j]+1
         vfields[j].add(field)
-
 
 good_tickets = []
 
@@ -101,20 +100,49 @@ unique_fields = set()
 for i in range(0,20):
     #print('Field ' + str(i)+ ': ')
     d = dict()
-    for ticket in good_tickets:
-        field = ticket[i]
-        unique_fields.add(field)
-        st = poss_fields(vfields,field)
-        for nm in st:
-            if not nm in d:
-                d[nm]=1
-            else:
-                d[nm] = d[nm] + 1
+    s = ''
+    for j in range(0, len(ranges)):
+        good = True
+        for ticket in good_tickets:
+            field = ticket[i]
+            if not ((field >= ranges[j][0] and field <= ranges[j][1] ) or (field >= ranges[j][2] and field <= ranges[j][3] )):
+              good = False
+              break
+        if good:
+            print('column ' + str(i) + ' could be field: ' + fields[j] )
+    print()
 
-        #num_poss_fields = len(poss_fields(vfields,field)) # all fields map to 19 or 20 possible fields
+print('analyze offline...')
+#print('min field: ' + str(minf)) # 48
+#print('max field: ' + str(maxf)) # 961
+#print('# good tickets: ' + str(len(good_tickets))) # 190
 
-
-print('min field: ' + str(minf)) # 48
-print('max field: ' + str(maxf)) # 961
-print('# good tickets: ' + str(len(good_tickets))) # 190
-print('unique_fields: ' + str(unique_fields))
+# After analyzing output...
+"""
+**COLUMN 0 could be field: departure track
+**COLUMN 1 could be field: seat
+**COLUMN 2 could be field: arrival station
+**COLUMN 3 could be field: departure date
+**COLUMN 4 could be field: departure location
+**COLUMN 5 could be field: class
+**COLUMN 6 could be field: duration
+**COLUMN 7 could be field: departure time
+**COLUMN 8 could be field: row
+**COLUMN 9 could be field: departure platform
+**COLUMN 10 could be field: arrival location
+**COLUMN 11 could be field: departure station
+**COLUMN 12 could be field: arrival platform
+**COLUMN 13 could be field: arrival track
+**COLUMN 14 could be field: type
+**COLUMN 15 could be field: train
+**COLUMN 16 could be field: price
+**COLUMN 17 could be field: zone
+**COLUMN 18 could be field: wagon
+**COLUMN 19 could be field: route
+"""
+my_ticket=[89,179,173,167,157,127,163,113,137,109,151,131,97,149,107,83,79,139,59,53]
+val = 1
+for i in range(0,len(my_ticket)):
+    if i in [0,3,4,7,9,11]:
+        val = val * my_ticket[i]
+print('part 2: ' + str(val))        
