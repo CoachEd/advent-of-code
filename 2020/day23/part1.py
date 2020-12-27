737e3b36732cc9a5020313077d584c4202972707
Ed Torres <CoachEd@gmail.com>
2020-12-27 14:46:14 -0500

    latest

---
+++ 2020/day23/part1.py
@@ -0,0 +1,68 @@
+
+import sys
+import time
+
+start_secs = time.time()
+
+s = '389125467' # sample
+s = '538914762' # puzzle input
+arr = [0] * (len(s)+1)
+for i in range(0,len(s)):
+  idx = int(s[i])  
+  if i == len(s)-1:
+    arr[idx] = int(s[0])
+  else:
+    arr[idx] = int(s[i+1])
+curr_val = int(s[0])  # 3
+
+
+moves = 100
+
+for i in range(1,moves+1):
+  curr1 = curr_val
+  s1 = 'move ' + str(i)+'  '
+  for i in range(1,len(arr)):
+    s1 = s1 + str(arr[curr1]) + ' '
+    curr1 = arr[curr1]
+  
+  # get curr, and next three vals
+  next_val1 = arr[curr_val]  # 8
+  next_val2 = arr[next_val1] # 9
+  next_val3 = arr[next_val2] # 1
+
+  # find dest
+  dest = curr_val - 1
+  while dest == 0 or dest == next_val1 or dest == next_val2 or dest == next_val3:
+    dest = dest - 1
+    if dest < 1:
+      dest = len(s)
+  
+  s1 = s1 + '  dest: ' + str(dest)
+  
+  print(s1)
+  
+  
+  # place cups
+  arr[curr_val] = arr[next_val3]
+  temp = arr[dest]
+  arr[dest] = next_val1
+  arr[next_val3] = temp
+  curr_val = arr[curr_val]
+
+
+print ('final')
+curr1 = curr_val
+s1 = ''
+for i in range(1,len(arr)):
+  s1 = s1 + str(arr[curr1]) + ' '
+  curr1 = arr[curr1]
+print(s1)
+n1=arr[1]
+n2=arr[n1]
+print(n1)
+print(n2)
+print(str(n1*n2))
+
+print()
+end_secs = time.time()
+print('--- ' + str(end_secs-start_secs)+ ' secs ---')
