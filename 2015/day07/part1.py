9adcd021d91e031def7a13f3c2aa96b96b4b3d3f
Ed Torres <CoachEd@gmail.com>
2020-12-27 23:13:50 -0500

    latest

--- 2015/day07/part1.py
+++ 2015/day07/part1.py
@@ -1,7 +1,14 @@
 import sys
 import time
+import numpy as np
 
 start_secs = time.time()
+
+def hasInstr(s):
+    if s.find('AND') != -1 or s.find('OR') != -1 or s.find('NOT') != -1 or s.find('LSHIFT') != -1 or s.find('RSHIFT') != -1:
+        return True
+    else:
+        return False
 
 # read in input file
 l=[]
@@ -11,57 +18,68 @@ for line in lines:
     l.append(line.strip())
 
 d = dict()
-for s in l:
-    arr = s.split('->')
-    dest = arr[1].strip()
-    arr2 = arr[0].split()
-    if arr2[0] == 'NOT':
-        operand = arr2[1].strip()
-        if operand in d:
-            d[dest] = ~d[operand]
-    elif len(arr2) == 1:
-        # assign value to wire
-        if arr2[0].isdigit():
-            d[dest] = int(arr2[0])
+i = 0
+while len(l) > 0:
+    if hasInstr(l[i]):
+        arr = l[i].split('->')
+        var = arr[-1].strip()
+        arr2 = arr[0].strip().split()
+        if l[i].find('NOT') != -1:
+            operand = arr2[1].strip()
+            if operand in d:
+                d[var] = ~d[operand]
+                del l[i]
         else:
-            if arr2[0] in d:
-                d[dest] = d[arr2[0]]
+            b1 = False
+            b2 = False
+            lft = arr2[0]
+            op = arr2[1]
+            rgt = arr2[2]
+            if lft.isdigit():
+                lft = int(lft)
+                b1 = True
+            if rgt.isdigit():
+                rgt = int(rgt)
+                b2 = True
+            if not b1:
+                b1 = lft in d
+                if b1:
+                    lft = d[lft]
+            if not b2:
+                b2 = rgt in d
+                if b2:
+                    rgt = d[rgt]
+            if b1 and b2:
+                # have operands, do cmd
+                if op == 'AND':
+                    d[var] = lft & rgt
+                elif op == 'OR':
+                    d[var] = lft | rgt
+                elif op == 'LSHIFT':
+                    d[var] = lft << rgt
+                elif op == 'RSHIFT':
+                    d[var] = lft >> rgt    
+                del l[i]        
     else:
-        b1 = False
-        b2 = False
-        lft = arr2[0]
-        if lft.isdigit():
-            lft = int(lft)
-            b1 = True
-        op = arr2[1]
-        rgt = arr2[2]
-        if rgt.isdigit():
-            rgt = int(rgt)
-            b2 = True
+        # no instr
+        arr = l[i].split('->')
+        var = arr[1].strip()
+        val = arr[0].strip()
+        if val.isdigit():
+            val = int(val)
+            d[var] = val
+            del l[i]
+        else:
+            if val in d:
+                d[var] = d[val]
+                del l[i]
         
-        if not b1:
-            b1 = lft in d
-            if b1:
-                lft = d[lft]
-        if not b2:
-            b2 = rgt in d
-            if b2:
-                rgt = d[rgt]
-        if b1 and b2:
-            # have operands, do cmd
-            if op == 'AND':
-                d[dest] = lft & rgt
-            elif op == 'OR':
-                d[dest] = lft | rgt
-            elif op == 'LSHIFT':
-                d[dest] = lft << rgt
-            elif op == 'RSHIFT':
-                d[dest] = lft >> rgt
+    i = i + 1
+    if i >= len(l):
+        i = 0
 
-                
-print(d)
-print('Part 1: ' + d['a'])
-        
+print('part 1: ' + str(np.uint16(d['a'])))
+
 
 end_secs = time.time()
 print(str(end_secs-start_secs))
