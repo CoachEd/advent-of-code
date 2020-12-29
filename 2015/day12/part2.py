e0f144492c7366cc015641a273ce9c11e32e1010
Ed Torres <ertorres@mitre.org>
2020-12-29 17:42:09 -0500

    latest

--- 2015/day12/part2.py
+++ 2015/day12/part2.py
@@ -6,9 +6,36 @@ import json
 start_secs = time.time()
 
 
+count = 0
 
-print()
+def recursive_iter(obj):
+    global count
 
+    if isinstance(obj, dict):
+        if 'red' in obj.values():
+            return
+        for k,v in obj.items():
+            if isinstance(v, dict):
+                recursive_iter(v)
+            elif isinstance(v, list):
+                for e in v:
+                    recursive_iter(e)
+            else:
+                s = str(v)
+                if s.isdigit():
+                    count = count + int(s)
+                if len(s) > 1 and s[0] == '-' and s[1:].isdigit():
+                    count = count + -1*int(s[1:])
+    elif isinstance(obj, list):
+        for elem in obj:
+            recursive_iter(elem)
+    else:
+        s = str(obj)
+        if s.isdigit():
+            count = count + int(s)
+        if len(s) > 1 and s[0] == '-' and s[1:].isdigit():
+            count = count + -1*int(s[1:])
+            
 # read in input file
 l=[]
 my_file = open("inp.txt", "r")
@@ -16,20 +43,11 @@ lines = my_file.readlines()
 for line in lines: 
     l.append(line.strip())
 
-# TODO
+# {"field1": "blue","field2": 1,"field3": [{"s1": 9,"s2": 8}, {"s1": 9,"s2": 8,"s3": "red"}, {"s1": 9,"s2": 8,"s3": 3}],"field4": 3}
 s = l[0]
-
-    
-
-
-arr = re.split('\[|\]|\{|\}|:|,',s)
-sum = 0
-for s in arr:
-    if s.isdigit():
-        sum = sum + int(s)
-    if len(s) > 1 and s[0] == '-' and s[1:].isdigit():
-        sum = sum + -1*int(s[1:])
-print('part 1: ' + str(sum)) 
+obj = json.loads(s)
+recursive_iter(obj)
+print('part 2: ' + str(count)) # 29291 too low
 
 print()
 end_secs = time.time()
