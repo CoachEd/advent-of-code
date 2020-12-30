4337e218f8a1caa4814f66b119a589d0814e7388
Ed Torres <CoachEd@gmail.com>
2020-12-29 19:37:35 -0500

    latest

--- 2015/day13/part1.py
+++ 2015/day13/part1.py
@@ -1,17 +1,22 @@
 import sys
 import time
+import itertools
 
 start_secs = time.time()
 print()
 
+d = dict()
+guests = ['Alice','Bob','Carol','David','Eric','Frank','George','Mallory']
+for i in range(0,len(guests)):
+    d[guests[i]] = i
+seating=list(itertools.permutations(guests))
+    
 # read in input file
 l=[]
 my_file = open("inp.txt", "r")
 lines = my_file.readlines()
 for line in lines: 
     l.append(line.strip())
-
-# TODO
 
 
 
