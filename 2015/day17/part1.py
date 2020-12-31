8eb8aeecde7be898622fc55a0fd757f8f15eab11
Ed Torres <ertorres@mitre.org>
2020-12-31 09:13:43 -0500

    latest

--- 2015/day17/part1.py
+++ 2015/day17/part1.py
@@ -1,18 +1,34 @@
 import sys
 import time
+from itertools import combinations
 
 start_secs = time.time()
 print()
 
 # read in input file
+
 l=[]
+arr=[]
 my_file = open("inp.txt", "r")
 lines = my_file.readlines()
 for line in lines: 
-    l.append(int(line.strip()))
+    line = line.strip()
+    arr.append(int(line))
 
-# fit 150 liters of egg nog
+liters=150
 
+count=0
+for i in range(4,len(arr)+1):
+    combos = combinations(arr, i)
+    for l in list(combos):
+        sum1=0
+        for x in l:
+            sum1=sum1+x
+        if sum1 == liters:
+            count=count+1
+        
+
+print('part 1: ' + str(count)) 
 
 
 print()
--- 2015/day17/part2.py
+++ 2015/day17/part2.py
@@ -1,18 +1,43 @@
 import sys
 import time
+from itertools import combinations
 
 start_secs = time.time()
 print()
 
 # read in input file
+
 l=[]
+arr=[]
 my_file = open("inp.txt", "r")
 lines = my_file.readlines()
 for line in lines: 
-    l.append(line.strip())
+    line = line.strip()
+    arr.append(int(line))
 
-# TODO
+liters=150
 
+count=0
+min_size = sys.maxsize
+keep = []
+for i in range(4,len(arr)+1):
+    combos = combinations(arr, i)
+    for l in list(combos):
+        sum1=0
+        sz = 0
+        for x in l:
+            sum1=sum1+x
+            sz = sz+1
+        if sum1 == liters:
+            keep.append(l)
+            if sz < min_size:
+                min_size = sz
+        
+count = 0
+for e in keep:
+    if len(e) == min_size:
+        count = count +1
+print('part 2: ' + str(count)) 
 
 
 print()
