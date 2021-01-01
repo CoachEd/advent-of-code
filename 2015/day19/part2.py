de6e4b8d79d337317248a576c4baf2f7b5d3ba19
Ed Torres <CoachEd@gmail.com>
2021-01-01 09:40:16 -0500

    latest

--- 2015/day19/part2.py
+++ 2015/day19/part2.py
@@ -1,6 +1,7 @@
 import sys
 import time
 import re
+import random
 
 start_secs = time.time()
 print()
@@ -12,26 +13,33 @@ lines = my_file.readlines()
 for line in lines: 
     l.append(line.strip())
 
+molecule=l[-1]
+del l[-1]
+del l[-1]
 # Al => ThF
+
+minsteps=sys.maxsize
+
 d = dict()
 for i in range(0,len(l)):
     s = l[i]
-    if len(s) == 0:
-        molecule = l[i+1]
+    arr = s.replace(' ','').split('=>')
+    d[arr[1]] = arr[0]
+    
+steps=0
+while True:
+    changes=False
+    for key, val in d.items():
+        if molecule.find(key) != -1:
+            steps=steps+1
+            changes=True
+        molecule=molecule.replace(key,val,1)
+        if molecule == 'e':
+            break
+    if not changes:
         break
-    else:
-        arr = s.replace(' ','').split('=>')
-        if not arr[0] in d:
-            d[arr[0]] = [arr[1]]
-        else:
-            d[arr[0]].append(arr[1])
-
-for key, val in d.items():
-    print(key + ' : ' + str(val))
-
-
-n = 0
-print('part 2: ' + str(n)) 
+print()
+print('part 2: ' + str(steps)) 
 
 
 
