f9281663741a08a7e486d82ee807cfbf6994f007
Ed Torres <CoachEd@gmail.com>
2021-01-01 19:07:25 -0500

    latest

--- 2015/day20/part2.py
+++ 2015/day20/part2.py
@@ -2,19 +2,31 @@ import sys
 import time
 
 start_secs = time.time()
-print()
 
-# read in input file
-l=[]
-my_file = open("inp.txt", "r")
-lines = my_file.readlines()
-for line in lines: 
-    l.append(line.strip())
+def factorize(num1):
+    return [n for n in range(1, num1 + 1) if num1 % n == 0]
 
-# TODO
+def num_presents(housenum):
+    l = factorize(housenum)
+    l2 =  [ i * 10 for i in l]
+    return sum(l2)
 
 
+n = 29000000
+house_num = -1
 
+for i in range(665280,665280+1):
+    presents = num_presents(i)
+    if presents >= n:
+        house_num = i
+        print('house_num: ' + str(house_num))
+
+
+
+print('part 1: ' + str(house_num))
+# too high: 800280
+# too high: 702240 
+# 665280 # YES!!
 print()
 end_secs = time.time()
 print('--- ' + str(end_secs-start_secs) + ' secs ---')
