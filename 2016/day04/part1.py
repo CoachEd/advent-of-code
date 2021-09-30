b08358525db86881d1afff32e5eb95532c6135cf
Edwin Torres <CoachEd@gmail.com>
2021-09-30 16:00:42 -0400

    16 day4 start

--- 2016/day04/part1.py
+++ 2016/day04/part1.py
@@ -12,7 +12,62 @@ for line in lines:
     l.append(line.strip())
 
 # TODO
-print(l)
+total = 0
+for s in l:
+  i1 = s.rindex('-')
+  i2 = s.rindex('[')
+  nm = s[0:i1]
+  sc = int(s[i1+1:i2])
+  cs = s[i2+1:len(s)-1]
+
+  # TEST: make sure all checksum characters are in name
+  d = {}
+  valid = True
+  for c in cs:
+    d[c] = nm.count(c)
+    if d[c] == 0:
+      valid = False
+      break
+  if not valid:
+    continue  # some checksum characters not found in name
+
+  # TEST: look at each letter of checksum; later counts cannot be bigger than earlier counts
+  first_time = True
+  prev_c = ''
+  prev_count = 0
+  last_pos = -1
+  last_count = -1
+  for c in cs:
+    i = nm.index(c)
+    if first_time:
+      # first time
+      first_time = False
+      prev_c = c
+      prev_count = d[c]
+    else:
+      if d[c] > prev_count:
+        valid = False  # later checksum count cannot be greater than earlier checksum count
+        break
+      prev_c = c
+      prev_count = d[c]
+    if i > last_pos:
+      last_pos = i
+      last_count = d[c]
+  if not valid:
+    continue
+ 
+  # TEST: Ties?
+
+
+
+
+  # OUTPUT result
+  print('valid: ' + nm)
+  # made it here, then valid!
+  total = total + sc
+
+print(total)  # WRONG: 50583 (too low), 280724 (too high)
+
 
 
 print('')
