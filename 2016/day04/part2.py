3f14a8888307587ec7de5712f8878c673b57d7ed
Edwin Torres <CoachEd@gmail.com>
2021-10-01 09:50:20 -0400

    day 4 done

---
+++ 2016/day04/part2.py
@@ -0,0 +1,83 @@
+import sys
+import time
+
+start_secs = time.time()
+print('')
+
+# read in input file
+l=[]
+my_file = open("inp.txt", "r")
+lines = my_file.readlines()
+for line in lines: 
+    l.append(line.strip())
+
+# TODO
+answer = ''
+for s in l:
+  i1 = s.rindex('-')
+  i2 = s.rindex('[')
+  nm = s[0:i1]
+  sc = int(s[i1+1:i2])
+  cs = s[i2+1:len(s)-1]
+
+  # TEST: make sure all checksum characters are in name
+  d = {}
+  valid = True
+  for c in cs:
+    d[c] = nm.count(c)
+    if d[c] == 0:
+      valid = False
+      break
+  if not valid:
+    continue  # some checksum characters not found in name
+
+  # TEST: look at each letter of checksum; later counts cannot be bigger than earlier counts
+  first_time = True
+  prev_c = ''
+  prev_count = 0
+  last_pos = -1
+  last_count = -1
+  for c in cs:
+    i = nm.index(c)
+    if first_time:
+      # first time
+      first_time = False
+      prev_c = c
+      prev_count = d[c]
+    else:
+      if d[c] > prev_count:
+        valid = False  # later checksum count cannot be greater than earlier checksum count
+        break
+      elif d[c] == prev_count and ord(prev_c) > ord(c):
+        valid = False  # tie, but later checksum car comes before earlier char alphabetically
+        break
+      prev_c = c
+      prev_count = d[c]
+    if i > last_pos:
+      last_pos = i
+      last_count = d[c]
+  if not valid:
+    continue
+
+  # made it here, then valid!
+  s1 = ''
+  for c in nm:
+    if c == '-':
+      s1 = s1 + ' '
+      continue
+    x = ord(c)
+    for i in range(0, sc):
+      x = x + 1
+      if x > 122:
+        x = 97
+    s1 = s1 + chr(x)
+
+  print(str(sc) + ": " + s1)
+  if s1.find('north') != -1:
+    answer = str(sc) + ": " + s1
+
+print('')
+print(answer)
+print('')
+end_secs = time.time()
+print('--- ' + str(end_secs-start_secs) + ' secs ---')
