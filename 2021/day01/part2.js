356500ccb0b81047d74c49ca86bee1f02975bdd9
Edwin Torres <CoachEd@gmail.com>
2021-12-01 17:31:46 -0500

    JS version

---
+++ 2021/day01/part2.js
@@ -0,0 +1,53 @@
+const fs = require('fs');
+const readline = require('readline');
+
+const getData = async (filename) => {
+  const fileStream = fs.createReadStream(filename);
+  const rl = readline.createInterface({
+    input: fileStream,
+    crlfDelay: Infinity
+  });
+  let arr = [];
+  for await (const line of rl) {
+    arr.push(line);
+  }
+  return arr;
+};
+
+const makeInts = (arr) => {
+  return arr.map(s => parseInt(s));
+};
+
+const makeThreeSumArr = (arr) => {
+  let arrtemp = [];
+  for (let i=0; i < arr.length-2; i++) {
+    let n = arr[i] + arr[i+1] + arr[i+2];
+    arrtemp.push(n); 
+  }
+  return arrtemp;
+};
+
+const increaseArr = (arr) => {
+  let arrtemp = [0];
+  for (let i=1; i < arr.length; i++) {
+    if (arr[i] > arr[i-1])
+      arrtemp.push(1);
+    else
+      arrtemp.push(0);
+  }
+  return arrtemp;
+}
+
+const add = (accumulator, a) => accumulator + a;
+
+const calcAnswer = (arr) => arr.reduce(add, 0);
+
+(async() => {
+  let arr2, answer;
+  arr2 = await getData('inp.txt');
+  arr2 = makeInts(arr2);
+  arr2 = makeThreeSumArr(arr2);  // new for part2
+  arr2 = increaseArr(arr2);
+  answer = calcAnswer(arr2);
+  console.log(answer);
+})();
