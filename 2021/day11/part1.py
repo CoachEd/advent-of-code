83dcd1d16b0015e02c3498aa383e5b4d09dda705
Edwin  Torres <coached@gmail.com>
2021-12-11 10:43:40 -0500

    latest

--- 2021/day11/part1.py
+++ 2021/day11/part1.py
@@ -1,7 +1,51 @@
+
+# Part 1
 """
 AoC
 """
 import time
+import sys
+from copy import copy, deepcopy
+
+flashes = 0
+def vnode(a,y,x):
+  if y >= 0 and y < len(a) and x >= 0 and x < len(a[0]):
+    return True
+  return False
+
+def fnb(a,y,x):
+  global flashes
+  ytl = y-1
+  xtl = x-1
+  yt = y-1
+  xt = x
+  ytr = y-1
+  xtr = x+1
+  yl = y
+  xl = x-1
+  yr = y
+  xr = x+1
+  ybl = y+1
+  xbl = x-1
+  yb = y+1
+  xb = x
+  ybr = y+1
+  xbr = x+1
+  nbrs = [ [ytl,xtl], [yt,xt], [ytr,xtr], [yl,xl], [yr,xr], [ybl,xbl], [yb,xb], [ybr,xbr] ]
+  flashed = []
+  for coord in nbrs:
+    y1 = coord[0]
+    x1 = coord[1]
+    if not vnode(a,y1,x1):
+      continue
+    if a[y1][x1] == 0:
+      continue
+    a[y1][x1] += 1
+    if a[y1][x1] > 9:
+      flashed.append([y1,x1])
+      a[y1][x1] = 0
+      flashes += 1
+  return flashed
 
 start_secs = time.time()
 print('')
@@ -13,9 +57,45 @@ lines = my_file.readlines()
 for line in lines:
   l.append(line.strip())
 
-print(l)
+a =[]
+for r in l:
+  row = []
+  for c in r:
+    row.append(int(c))
+  a.append(row)
 
+steps = 100
+for i in range(steps):
+  a1 = deepcopy(a)
+  
+  # increase all by 1
+  for y in range(len(a1)):
+    for x in range(len(a1[y])):
+      a1[y][x] += 1
+      
+  flashed = []
+  for y in range(len(a1)):
+    for x in range(len(a1[y])):
+      if a1[y][x] > 9:
+        flashed.append([y,x])
+        a1[y][x] = 0
+        flashes += 1
+        
+  while len(flashed) > 0:
+    flashed2 =[]
+    for coord in flashed:
+      flashed2 += fnb(a1,coord[0],coord[1])
+    flashed = flashed2.copy()
+      
+  a = deepcopy(a1)
+      
+
+print(flashes)
 
 print('')
 end_secs = time.time()
 print('--- ' + str(end_secs-start_secs) + ' secs ---')
+
+
+
+
