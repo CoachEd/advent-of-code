5e658ba616397396a2e5b4ede62c916ab4cd388f
Edwin  Torres <coached@gmail.com>
2021-12-14 07:19:34 -0500

    part1.py

--- 2021/day14/part1.py
+++ 2021/day14/part1.py
@@ -1,7 +1,13 @@
+# part 1
+
+
 """
 AoC
 """
 import time
+import sys
+from copy import copy, deepcopy
+d = {}
 
 start_secs = time.time()
 print('')
@@ -13,9 +19,42 @@ lines = my_file.readlines()
 for line in lines:
   l.append(line.strip())
 
-print(l)
+s = l[0]
+for i in range(2,len(l)):
+  a = l[i].split(' -> ')
+  d[ a[0] ] = a[1]
 
+#print(s)
+for step in range(  10 ):
+  s1 = []
+  for c in s:
+    s1 += ([c] + [''])
+  
+  for i in range(0,len(s1)-3,2):
+    ss = s1[i] + s1[i+2]
+    if ss in d:
+      c = d[ss]
+      s1[i+1] = c
+  s = '%s' % ''.join(s1)
+  #print('step ' + str(step+1)+': ' + str(len(s)))
+  #print(s)
+  
+  
 
-print('')
+d = {}
+for c in s:
+  if not c in d:
+    d[c]=0
+  d[c] += 1
+
+mx = 0
+mn = sys.maxsize
+for key in d:
+  if d[key] > mx:
+    mx = d[key]
+  if d[key] < mn:
+    mn = d[key]
+print(str(mx-mn))
+
 end_secs = time.time()
 print('--- ' + str(end_secs-start_secs) + ' secs ---')
