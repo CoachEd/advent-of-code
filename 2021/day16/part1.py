8bb1cac93e0059c620e6daaccbf69f92078cd10c
Edwin Torres <CoachEd@gmail.com>
2021-12-16 07:28:50 -0500

    latest

--- 2021/day16/part1.py
+++ 2021/day16/part1.py
@@ -5,6 +5,8 @@ import time
 import sys
 from copy import copy, deepcopy
 
+version_number_sum = 0
+
 def hex2bin(h):
   int_value = int(h, base=16)
   bin_value = bin(int_value)
@@ -13,26 +15,73 @@ def hex2bin(h):
     s = '0' + s
   return s
 
-def litval(bstr):
-  # packet header
-  packet_version = int(bstr[0:3],2)
-  packet_type_id = int(bstr[3:6],2) # 4 - LITERAL VALUE
-  if packet_type_id != 4:
-    print('ERROR- not a LIT VALUE: ' + str(bstr))
+def operator(b):
+  global version_number_sum
+  i = 0
+  packet_version = int(b[i:i+3],2)
+  version_number_sum += packet_version
+  packet_type_id = int(b[i+3:i+6],2)
+  i += 6  
+  length_type_id = b[i]  # I == 0, 15-bit number, number of bits in sub-packets
+  if length_type_id == '0':
+    # mode 0
+    length_of_subpackets = b[i+1:i+16]   # 27
+    subpacket1 = b[i+16:i+27] # 110 100 01010 parses to: 10
+    subpacket2 = b[i+27:i+43] # 010 100 10001 00100 parses to: 20
+    return (litval(subpacket1), litval(subpacket2), 56)
+  elif length_type_id == '1':
+    # mode 1
+    # 111 011 1 00000000011 01010000001 10010000010 00110000011 00000
+    # VVV TTT I LLLLLLLLLLL AAAAAAAAAAA BBBBBBBBBBB CCCCCCCCCCC
+    num_subpackets = int(b[i+1:i+12],2)
+    litvals =[0 for i in range(num_subpackets)]
+    for j in range(num_subpackets):
+      subpacket = b[i+12:i+23]
+      litvals[j] = litval(subpacket)
+      i += 11
+    return (litvals, 'ignore', 23+(num_subpackets*11))
+  else:
+    print('error length_type_id: ' + str(length_type_id))
     sys.exit()
-  i = 6
+
+def litval(b):
+  global version_number_sum
+  i = 0
+  len1 = 0
+  packet_version = int(b[i:i+3],2)
+  version_number_sum += packet_version
+  packet_type_id = int(b[i+3:i+6],2)
+  i += 6
   last_group = False
   bstr2 = ''
   while not last_group:
-    group_ind = bstr[i]  # starts with 1, not the last group 
+    group_ind = b[i]  # starts with 1, not the last group 
     if group_ind != '1':
       last_group = True
-    group = bstr[i+1:i+5]  # 4 bit group
+    group = b[i+1:i+5]  # 4 bit group
     bstr2 += group
     i = i + 5
-
+    len1 += 5
+  len1 += 6 + 3 # header and ignore
   value = int(bstr2,2)
-  return value
+  return ( value, len1 )
+
+def sum_version_numbers(b):
+  i = 0
+  blen = len(b)
+  while True:
+    packet_type_id = int(b[i+3:i+6],2)
+    if packet_type_id == 4:
+      # literval value
+      (value , len1) = litval(b[i:])
+      #print(value)
+    else:
+      (value1, value2, len1) = operator(b[i:])
+      #print('here1: ' + str(value1) + ',' + str(value2) + ',' + str(len1))
+
+    i += len1
+    if i >= blen:
+      break
 
 start_secs = time.time()
 print('')
@@ -44,46 +93,11 @@ lines = my_file.readlines()
 for line in lines:
   l.append(line.strip())
 
-
-# literal value example
-# 110 100 10111 11110 00101 000
-print('')
-print('OPERATOR example:')
-b = hex2bin('D2FE28')
-
-# packet header
-packet_version = int(b[0:3],2)
-packet_type_id = int(b[3:6],2) # 4 - LITERAL VALUE
-
-group_ind = b[6]  # starts with 1, not the last group 
-group1 = b[7:11]  # 4 bit group
-group_ind = b[11]  # starts with 1, not the last group 
-group2 = b[12:16]  # 4 bit group
-group_ind = b[16]  # starts with 0, last group 
-group3 = b[17:21]  # 4 bit group
-ignore_group = b[21:]
-value = int(group1+group2+group3,2) # 2021
-
-
-# operator example
-# 001 110 0 000000000011011 11010001010 0101001000100100 0000000
-# VVV TTT I LLLLLLLLLLLLLLL AAAAAAAAAAA BBBBBBBBBBBBBBBB
-print('')
-print('OPERATOR example:')
-b = hex2bin('38006F45291200')
-
-# packet header
-packet_version = int(b[0:3],2) # 1
-packet_type_id = int(b[3:6],2) # 6 - OPERATOR
-length_type_id = b[6]  # I == 0, 15-bit number, number of bits in sub-packets
-length_of_subpackets = b[7:22]   # 27
-subpacket1 = b[22:33] # 110 100 01010 parses to: 10
-subpacket2 = b[33:49] # 010 100 10001 00100 parses to: 20
-
-print(litval(subpacket1))
-print(litval(subpacket2))
-
-
+s = hex2bin(l[0])
+print(s)
+sys.exit()
+sum_version_numbers(s)
+print(version_number_sum)
 
 end_secs = time.time()
 print('--- ' + str(end_secs-start_secs) + ' secs ---')
