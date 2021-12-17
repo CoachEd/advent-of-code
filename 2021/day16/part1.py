fac40bf50c157ee6a60ebf67dc1ff0e82469786e
Edwin Torres <CoachEd@gmail.com>
2021-12-16 23:47:28 -0500

    latest

--- 2021/day16/part1.py
+++ 2021/day16/part1.py
@@ -5,7 +5,45 @@ import time
 import sys
 from copy import copy, deepcopy
 
-version_number_sum = 0
+tot_version_nums = 0
+
+def process_packet(b):
+  global tot_version_nums
+  i = 0
+  packet_version = int(b[i:i+3],2)
+  tot_version_nums += packet_version
+  packet_type_id = int(b[i+3:i+6],2)
+  i += 6
+  if packet_type_id == 4:
+    # literal value
+    # process groups
+    last_group = False
+    b2 = ''
+    while not last_group:
+      group_ind = b[i]  # starts with 1, not the last group 
+      if group_ind != '1':
+        last_group = True
+      group = b[i+1:i+5]  # 4 bit group
+      b2+= group
+      i = i + 5
+    value = int(b2,2)
+    #print(value) #KEEP
+    return b[i:]
+  else:
+    # operator
+    length_type_id = int(b[i],2)
+    i += 1
+    if length_type_id == 0:
+      total_length_bits = int(b[i:i+15],2)
+      i += 15
+      return process_packet(b[i:i+total_length_bits]) + b[i+total_length_bits:]
+    elif length_type_id == 1:
+      num_subpackets = int(b[i:i+11],2)
+      i += 11
+      sp = b[i:]
+      for n in range(num_subpackets):
+        sp = process_packet(sp)
+      return sp
 
 def hex2bin(h):
   int_value = int(h, base=16)
@@ -14,80 +52,6 @@ def hex2bin(h):
   while len(s) % 4 != 0:
     s = '0' + s
   return s
-
-def opval(b):
-  global version_number_sum
-  packet_version = int(b[0:3],2)
-  version_number_sum += packet_version
-  packet_type_id = int(b[3:6],2)
-  length_type_id = b[6]  # I == 0, 15-bit number, number of bits in sub-packets
-  if length_type_id == '0':
-    # mode 0
-    #001110 0 000000000011011 11010001010 0101001000100100 0000000
-    #                         11010001010 0101001000100100
-    #VVVTTT I LLLLLLLLLLLLLLL AAAAAAAAAAA BBBBBBBBBBBBBBBB
-    length_of_subpackets = int(b[7:22],2)
-    b1 = b[22:22+length_of_subpackets]
-    return b1
-  elif length_type_id == '1':
-    # mode 1
-    # 111 011 1 00000000011 01010000001 10010000010 00110000011 00000
-    # VVV TTT I LLLLLLLLLLL AAAAAAAAAAA BBBBBBBBBBB CCCCCCCCCCC
-    num_subpackets = int(b[7:18],2)
-    litvals =[0 for i in range(num_subpackets)]
-    for j in range(num_subpackets):
-      subpacket = b[18:29]
-      litvals[j] = litval(subpacket)
-    return (litvals, 'ignore', 23+(num_subpackets*11))
-
-def litval(b):
-  global version_number_sum
-  if len(b) == 0:
-    return ( 0, '' )
-  i = 0
-  packet_version = int(b[i:i+3],2)
-  version_number_sum += packet_version
-  packet_type_id = int(b[i+3:i+6],2)
-  i += 6
-  # 0011100000000000011011 11010001010 0101001000100100 0000000
-  # VVVTTTILLLLLLLLLLLLLLL AAAAAAAAAAA BBBBBBBBBBBBBBBB
-  last_group = False
-  bstr2 = ''
-  while not last_group:
-    group_ind = b[i]  # starts with 1, not the last group 
-    if group_ind != '1':
-      last_group = True
-    group = b[i+1:i+5]  # 4 bit group
-    bstr2 += group
-    i = i + 5
-
-  # any bits to ignore?
-  value = int(bstr2,2)
-  retval = b[i:]
-  rest = b[i:].replace('0','')
-  if len(rest) == 0:
-    retval = rest
-  print(retval)
-  return ( value, retval )
-
-"""
-def sum_version_numbers(b):
-  i = 0
-  blen = len(b)
-  while True:
-    packet_type_id = int(b[i+3:i+6],2)
-    if packet_type_id == 4:
-      # literval value
-      (value , len1) = litval(b[i:])
-      #print(value)
-    else:
-      (value1, value2, len1) = operator(b[i:])
-      #print('here1: ' + str(value1) + ',' + str(value2) + ',' + str(len1))
-
-    i += len1
-    if i >= blen:
-      break
-"""
 
 start_secs = time.time()
 print('')
@@ -99,39 +63,15 @@ lines = my_file.readlines()
 for line in lines:
   l.append(line.strip())
 
-
-def parse_bin(s):
-  if len(s) == 0:
-    return ''
-  packet_version = int(s[0:3],2)
-  packet_type_id = int(s[3:6],2)
-  if packet_type_id == 4:
-    # literval value
-    (value, s) = litval(s)
-    print('value: ' + str(value) + '  s: ' + s)
-    return s
-  else:
-    # operator
-    # TODO
-    s = opval(s)
-    return s
-
 # main
-s = hex2bin(l[0])
-while len(s) > 0:
-  s = parse_bin(s)
-
+b = hex2bin(l[0])
+while True:
+  b = process_packet(b)
+  len1 = len(b)
+  if len1 == 0 or len1 == b.count('0'):
+    break
   
-
-
-
-
-
-
-"""
-sum_version_numbers(s)
-print(version_number_sum)
-"""
+print(tot_version_nums)
 
 end_secs = time.time()
 print('--- ' + str(end_secs-start_secs) + ' secs ---')
