f33179ec035aabe8c8e51519776955f6f9dc1246
Edwin Torres <CoachEd@gmail.com>
2021-12-17 06:55:48 -0500

    latest

--- 2021/day16/part2.py
+++ 2021/day16/part2.py
@@ -5,13 +5,9 @@ import time
 import sys
 from copy import copy, deepcopy
 
-tot_version_nums = 0
-
 def process_packet(b):
-  global tot_version_nums
   i = 0
   packet_version = int(b[i:i+3],2)
-  tot_version_nums += packet_version
   packet_type_id = int(b[i+3:i+6],2)
   i += 6
   if packet_type_id == 4:
@@ -27,29 +23,72 @@ def process_packet(b):
       b2+= group
       i = i + 5
     value = int(b2,2)
-    #print(value) #KEEP
-    return b[i:]
+    return (value, b[i:])
   else:
     # operator
     length_type_id = int(b[i],2)
     i += 1
+    values = []
+    rest = ''
     if length_type_id == 0:
       total_length_bits = int(b[i:i+15],2)
       i += 15
-      return process_packet(b[i:i+total_length_bits]) + b[i+total_length_bits:]
+      subpackets = b[i:i+total_length_bits]
+      rest = b[i+total_length_bits:]
+      while True:
+        (x,subpackets) = process_packet(subpackets)
+        values.append(x)
+        if len(subpackets) == 0:
+          break
     elif length_type_id == 1:
       num_subpackets = int(b[i:i+11],2)
       i += 11
-      sp = b[i:]
+      subpackets = b[i:]
       for n in range(num_subpackets):
-        sp = process_packet(sp)
-      return sp
+        (x, subpackets) = process_packet(subpackets)
+        values.append(x)
+      rest = subpackets
+    
+    if packet_type_id == 0:
+      # sum
+      result = 0
+      for x in values:
+        result += x
+    elif packet_type_id == 1:
+      # product
+      result = 1
+      for x in values:
+        result *= x
+    elif packet_type_id == 2:
+      values.sort()
+      result = values[0]
+    elif packet_type_id == 3:
+      values.sort()
+      result = values[-1]
+    elif packet_type_id == 5:
+      if values[0] > values[1]:
+        result = 1
+      else:
+        result = 0
+    elif packet_type_id == 6:
+      if values[0] < values[1]:
+        result = 1
+      else:
+        result = 0
+    elif packet_type_id == 7:
+      if values[0] == values[1]:
+        result = 1
+      else:
+        result = 0        
+    return(result, rest)
+
 
 def hex2bin(h):
+  len1 = len(h*4)
   int_value = int(h, base=16)
   bin_value = bin(int_value)
   s = str(bin_value)[2:]
-  while len(s) % 4 != 0:
+  while len(s) != len1:
     s = '0' + s
   return s
 
@@ -65,13 +104,9 @@ for line in lines:
 
 # main
 b = hex2bin(l[0])
-while True:
-  b = process_packet(b)
-  len1 = len(b)
-  if len1 == 0 or len1 == b.count('0'):
-    break
-  
-print(tot_version_nums)
+(value, b) = process_packet(b)
+print(value)
+print('')
 
 end_secs = time.time()
 print('--- ' + str(end_secs-start_secs) + ' secs ---')
