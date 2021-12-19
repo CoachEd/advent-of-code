6f870a0d5d9273169aefaf22dd3c12bace96c65a
Edwin  Torres <coached@gmail.com>
2021-12-19 18:12:30 -0500

    load data

--- 2021/day19/part1.py
+++ 2021/day19/part1.py
@@ -9,13 +9,30 @@ start_secs = time.time()
 print('')
 
 # read in input file
-l=[]
+d=[]
 my_file = open("inp.txt", "r", encoding='utf-8')
 lines = my_file.readlines()
+scanner_num = 0
+points = []
 for line in lines:
-  l.append(line.strip())
+  # --- scanner 0 ---
+  s = line.strip()
+  if s.startswith('---'):
+    continue
+  if len(s) == 0:
+    d.append(points)
+    points = []
+    scanner_num += 1
+    continue
+  arr = [ int(x) for x in s.split(',')]
+  points.append(arr)
+d.append(points)
+  
 
-print(l)
+  
+  
+  
+
 
 
 print('')
