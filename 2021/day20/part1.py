101c88c413cf00edff0f22573afbb8144dca1386
Edwin Torres <CoachEd@gmail.com>
2021-12-20 14:10:00 -0500

    latest

--- 2021/day20/part1.py
+++ 2021/day20/part1.py
@@ -6,6 +6,19 @@ import sys
 from copy import copy, deepcopy
 import math
 
+
+def count_lights2(m):
+  s = ''
+  n = 0
+  for r in m:
+    for c in r:
+      s += c
+      if c == '#':
+        n += 1
+    s += '\n'
+  print(s)
+  return n
+
 def count_lights(m):
   n = 0
   for r in m:
@@ -58,6 +71,7 @@ rmult = 3
 cmult = 3
 num_rows = len(l) * rmult
 num_cols = len(l[0]) * cmult
+
 m = [ [ '.' for r in range(num_rows) ] for c in range(num_cols) ]
 midy = math.floor(num_rows / 2) - math.floor(len(l)/2)
 midx = math.floor(num_cols / 2) - math.floor(len(l[0])/2)
@@ -70,8 +84,8 @@ for r in l:
     x += 1
   y += 1
 
+# main
 reps = 2
-
 print_m(m)
 for i in range(reps):
   m1 = deepcopy(m)
@@ -86,7 +100,7 @@ for i in range(reps):
   m = deepcopy(m1)
   print_m(m1)
 
-print( count_lights(m) )
+print( count_lights2(m) )
 
 # 5255 too high
 
