4d2a885c4b0edd85ac691f88d8bacb957559f36d
Edwin Torres <CoachEd@gmail.com>
2021-12-21 12:48:18 -0500

    latest

---
+++ 2021/day21/part2.py
@@ -0,0 +1,84 @@
+"""
+AoC
+"""
+import time
+import sys
+from copy import copy, deepcopy
+
+start_secs = time.time()
+print('')
+
+board_len = 10
+board = [ (n+1) for n in range(board_len)]
+
+di = 0
+dice = [ x for x in range(1,101) ]
+def roll():
+  global di
+  global dice
+  n = dice[di]
+  di += 1
+  if di >= len(dice):
+    di = 0
+  return n
+
+def move_forward(p,n):
+  global pstart
+  global board_len
+  global board
+
+  curr_pos = pstart[p-1]
+  end_pos = (curr_pos + n) % 10
+  pstart[p-1] = end_pos
+  return end_pos
+
+# read in input file
+l=[]
+my_file = open("inp.txt", "r", encoding='utf-8')
+lines = my_file.readlines()
+for line in lines:
+  l.append(line.strip())
+
+pstart = []
+for s in l:
+  arr = s.split(':')
+  pstart.append(int(arr[1])-1)
+pscores = [0,0]
+
+player = 1
+rolls = 0
+while True:
+  r1 = roll()
+  r2 = roll()
+  r3 = roll()
+
+  #print('Player ' + str(player) + ': ' + str(r1) + '+'+str(r2) + '+' + str(r3))
+
+  r = r1 + r2 + r3
+  rolls += 3
+  spot = move_forward(player,r)
+  pscores[player-1] += board[spot]
+  if player == 2:
+    player = 1
+  else:
+    player = 2  
+  if pscores[0] >= 1000 or pscores[1] >= 1000:
+    break
+
+# result
+if pscores[0] < pscores[1]:
+  print(rolls * pscores[0])
+else:
+  print(rolls * pscores[1])
+
+
+
+  
+
+
+
+
+
+print('')
+end_secs = time.time()
+print('--- ' + str(end_secs-start_secs) + ' secs ---')
