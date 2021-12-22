2ba52a50d937ae15f10cfca2a72282f767df02a0
Edwin  Torres <coached@gmail.com>
2021-12-21 19:28:34 -0500

    part2 start

--- 2021/day21/part2.py
+++ 2021/day21/part2.py
@@ -11,48 +11,6 @@ print('')
 board_len = 10
 board = [ (n+1) for n in range(board_len)]
 
-winners = [0,0]
-def quantum_roll(player_turn,pstart,pscores):
-  global winners
-  global board
-  if pscores[0] >= 21:
-    winners[0] += 1
-    return
-  if pscores[1] >= 21:
-    winners[1] += 1
-    return
-
-  pstart1 = pstart.copy()
-  pstart2 = pstart.copy()
-  pstart3 = pstart.copy()
-
-  pscores1 = pscores.copy()
-  pscores2 = pscores.copy()
-  pscores3 = pscores.copy()
-
-  (spot,pstart1) = move_forward(player_turn,1,pstart1)
-  pscores1[player_turn-1] += 1
-
-  (spot,pstart2) = move_forward(player_turn,2,pstart2)
-  pscores2[player_turn-1] += 2
-
-  (spot,pstart3) = move_forward(player_turn,3,pstart3)
-  pscores3[player_turn-1] += 3    
-
-  next_player = -1
-  if player_turn == 1:
-    next_player = 2
-  else:
-    next_player = 1
-  
-  quantum_roll(player_turn,pstart1,pscores1)
-  quantum_roll(player_turn,pstart2,pscores2)
-  quantum_roll(player_turn,pstart3,pscores3)
-
-
-
-
-
 di = 0
 dice = [ x for x in range(1,101) ]
 def roll():
@@ -71,7 +29,26 @@ def move_forward(p,n,pstart):
   curr_pos = pstart[p-1]
   end_pos = (curr_pos + n) % 10
   pstart[p-1] = end_pos
-  return (end_pos,pstart)
+  return end_pos
+
+def play_game(pstart,pscores):
+  player = 1
+  rolls = 0
+  while True:
+    r1 = roll()  # TODO: this should open parallel universe with roll 1
+    r2 = roll()  # TODO: this should open parallel universe with roll 1
+    r3 = roll()  # TODO: this should open parallel universe with roll 1
+    r = r1 + r2 + r3
+    rolls += 3
+    spot = move_forward(player,r,pstart)
+    pscores[player-1] += board[spot]
+    if player == 2:
+      player = 1
+    else:
+      player = 2  
+    if pscores[0] >= 1000 or pscores[1] >= 1000:
+      break
+  return (pscores, rolls)
 
 # read in input file
 l=[]
@@ -86,38 +63,13 @@ for s in l:
   pstart.append(int(arr[1])-1)
 pscores = [0,0]
 
-quantum_roll(1,pstart,pscores)
-
-
-print(winners)
-
-"""
-player = 1
-rolls = 0
-while True:
-  r1 = roll()
-  r2 = roll()
-  r3 = roll()
-
-  #print('Player ' + str(player) + ': ' + str(r1) + '+'+str(r2) + '+' + str(r3))
-
-  r = r1 + r2 + r3
-  rolls += 3
-  spot = move_forward(player,r)
-  pscores[player-1] += board[spot]
-  if player == 2:
-    player = 1
-  else:
-    player = 2  
-  if pscores[0] >= 1000 or pscores[1] >= 1000:
-    break
+(pscores, rolls) = play_game(pstart,pscores)
 
 # result
 if pscores[0] < pscores[1]:
   print(rolls * pscores[0])
 else:
   print(rolls * pscores[1])
-"""
 
 
 
