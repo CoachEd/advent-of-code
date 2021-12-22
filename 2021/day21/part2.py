cb774f3256811db34f46a0f07a54a428a0078d57
Edwin Torres <CoachEd@gmail.com>
2021-12-22 12:33:20 -0500

    latest

--- 2021/day21/part2.py
+++ 2021/day21/part2.py
@@ -24,61 +24,70 @@ def roll():
     di = 0
   return n
 
-def move_forward(p,n,pstart):
+def move_forward(p,n,p1_pos,p2_pos):
   global board_len
   global board
 
-  curr_pos = pstart[p-1]
+  curr_pos = p1_pos
+  if p == 2:
+    curr_pos = p2_pos
+
   end_pos = (curr_pos + n) % 10
-  pstart[p-1] = end_pos
   return end_pos
 
-def play_game(player,dice,pstart,pscores):
+def play_game(player,dice1,dice2,dice3,p1_pos,p2_pos,p1_score,p2_score):
   global wins
-  if pscores[0] >= 21:
+  if p1_score >= 21:
     wins[0] += 1
     return
-  if pscores[1] >= 21:
+  if p2_score >= 21:
     wins[1] += 1
     return
   
-  if not -1 in dice:
-    r = dice[0] + dice[1] + dice[2]
-    spot = move_forward(player,r,pstart)
-    pscores[player-1] += board[spot]
-    if pscores[player-1] >= 21:
-      wins[player-1] += 1
+  if dice1 != -1 and dice2 != -1 and dice3 != -1:
+    r = dice1 + dice2 + dice3
+    spot = move_forward(player,r,p1_pos,p2_pos)
+    if player == 1:
+      p1_pos = spot
+      p1_score += board[spot]
+    else:
+      p2_pos = spot
+      p2_score += board[spot]
+
+    if p1_score >= 21:
+      wins[0] += 1
       return
-    dice[0] = -1
-    dice[1] = -1
-    dice[2] = -1
+    elif p2_score >= 21:
+      wins[1] += 1
+      return
+    
+    dice1 = -1
+    dice2 = -1
+    dice3 = -1
     # next turn
     if player == 1:
       player = 2
     else:
       player = 1
-    play_game(player,dice,pstart.copy(),pscores.copy())
+    play_game(player,dice1,dice2,dice3,p1_pos,p2_pos,p1_score,p2_score)
   else:
     # roll 1, 2, 3
-    dice1 = dice.copy()
-    dice2 = dice.copy()
-    dice3 = dice.copy()
-    if dice[0] == -1:
-      dice1[0] = 1
-      dice2[0] = 2
-      dice3[0] = 3
-    elif dice[1] == -1:
-      dice1[1] = 1
-      dice2[1] = 2
-      dice3[1] = 3
-    elif dice[2] == -1:
-      dice1[2] = 1
-      dice2[2] = 2
-      dice3[2] = 3      
-
-    play_game(player,dice1,pstart.copy(),pscores.copy())
-    play_game(player,dice2,pstart.copy(),pscores.copy())
-    play_game(player,dice3,pstart.copy(),pscores.copy())
+    if dice1 == -1:
+      play_game(player,1,dice2,dice3,p1_pos,p2_pos,p1_score,p2_score)
+      play_game(player,2,dice2,dice3,p1_pos,p2_pos,p1_score,p2_score)
+      play_game(player,3,dice2,dice3,p1_pos,p2_pos,p1_score,p2_score)
+    elif dice2 == -1:
+      play_game(player,dice1,1,dice3,p1_pos,p2_pos,p1_score,p2_score)
+      play_game(player,dice1,2,dice3,p1_pos,p2_pos,p1_score,p2_score)
+      play_game(player,dice1,3,dice3,p1_pos,p2_pos,p1_score,p2_score)
+    elif dice3 == -1:
+      dice3 = 3      
+      play_game(player,dice1,dice2,1,p1_pos,p2_pos,p1_score,p2_score)
+      play_game(player,dice1,dice2,2,p1_pos,p2_pos,p1_score,p2_score)
+      play_game(player,dice1,dice2,3,p1_pos,p2_pos,p1_score,p2_score)
+    
+    
+    
   return
 
 
@@ -95,7 +104,7 @@ for s in l:
   pstart.append(int(arr[1])-1)
 pscores = [0,0]
 
-play_game(1,[-1,-1,-1],pstart,pscores)
+play_game(1,-1,-1,-1,pstart[0],pstart[1],pscores[0],pscores[1])
 
 print(wins)
 
