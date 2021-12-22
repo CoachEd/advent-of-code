4306a06be4c298f7a287ae0aa5e533c0654898c6
Edwin Torres <CoachEd@gmail.com>
2021-12-22 15:54:20 -0500

    latest

--- 2021/day21/part2.py
+++ 2021/day21/part2.py
@@ -8,8 +8,6 @@ from copy import copy, deepcopy
 start_secs = time.time()
 print('')
 
-wins = [0,0]
-
 board_len = 10
 board = [ (n+1) for n in range(board_len)]
 
@@ -24,72 +22,15 @@ def roll():
     di = 0
   return n
 
-def move_forward(p,n,p1_pos,p2_pos):
+def move_forward(p,n):
+  global pstart
   global board_len
   global board
 
-  curr_pos = p1_pos
-  if p == 2:
-    curr_pos = p2_pos
-
+  curr_pos = pstart[p-1]
   end_pos = (curr_pos + n) % 10
+  pstart[p-1] = end_pos
   return end_pos
-
-def play_game(player,dice1,dice2,dice3,p1_pos,p2_pos,p1_score,p2_score):
-  global wins
-  if p1_score >= 21:
-    wins[0] += 1
-    return
-  if p2_score >= 21:
-    wins[1] += 1
-    return
-  
-  if dice1 != -1 and dice2 != -1 and dice3 != -1:
-    r = dice1 + dice2 + dice3
-    spot = move_forward(player,r,p1_pos,p2_pos)
-    if player == 1:
-      p1_pos = spot
-      p1_score += board[spot]
-    else:
-      p2_pos = spot
-      p2_score += board[spot]
-
-    if p1_score >= 21:
-      wins[0] += 1
-      return
-    elif p2_score >= 21:
-      wins[1] += 1
-      return
-    
-    dice1 = -1
-    dice2 = -1
-    dice3 = -1
-    # next turn
-    if player == 1:
-      player = 2
-    else:
-      player = 1
-    play_game(player,dice1,dice2,dice3,p1_pos,p2_pos,p1_score,p2_score)
-  else:
-    # roll 1, 2, 3
-    if dice1 == -1:
-      play_game(player,1,dice2,dice3,p1_pos,p2_pos,p1_score,p2_score)
-      play_game(player,2,dice2,dice3,p1_pos,p2_pos,p1_score,p2_score)
-      play_game(player,3,dice2,dice3,p1_pos,p2_pos,p1_score,p2_score)
-    elif dice2 == -1:
-      play_game(player,dice1,1,dice3,p1_pos,p2_pos,p1_score,p2_score)
-      play_game(player,dice1,2,dice3,p1_pos,p2_pos,p1_score,p2_score)
-      play_game(player,dice1,3,dice3,p1_pos,p2_pos,p1_score,p2_score)
-    elif dice3 == -1:
-      dice3 = 3      
-      play_game(player,dice1,dice2,1,p1_pos,p2_pos,p1_score,p2_score)
-      play_game(player,dice1,dice2,2,p1_pos,p2_pos,p1_score,p2_score)
-      play_game(player,dice1,dice2,3,p1_pos,p2_pos,p1_score,p2_score)
-    
-    
-    
-  return
-
 
 # read in input file
 l=[]
@@ -102,11 +43,44 @@ pstart = []
 for s in l:
   arr = s.split(':')
   pstart.append(int(arr[1])-1)
+
 pscores = [0,0]
+player = 1
+rolls = 0
+temp_rolls = 0
+r = 0
+while True:
+  r += roll()
+  temp_rolls += 1
+  if temp_rolls < 3:
+    continue
+  
+  temp_rolls = 0
+  rolls += 3
 
-play_game(1,-1,-1,-1,pstart[0],pstart[1],pscores[0],pscores[1])
+  spot = move_forward(player,r)
+  pscores[player-1] += board[spot]
+  if player == 2:
+    player = 1
+  else:
+    player = 2 
+  r = 0
+  if pscores[0] >= 1000 or pscores[1] >= 1000:
+    break
 
-print(wins)
+# result
+if pscores[0] < pscores[1]:
+  print(rolls * pscores[0])
+else:
+  print(rolls * pscores[1])
+
+
+
+  
+
+
+
+
 
 print('')
 end_secs = time.time()
