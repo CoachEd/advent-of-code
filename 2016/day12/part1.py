81bac4d1e92063e78bbade8a7717e251da61039f
Edwin Torres <CoachEd@gmail.com>
2022-01-06 14:33:37 -0500

    latest

--- 2016/day12/part1.py
+++ 2016/day12/part1.py
@@ -3,6 +3,7 @@ AoC
 """
 import time
 import sys
+import math
 from copy import copy, deepcopy
 
 start_secs = time.time()
@@ -15,8 +16,47 @@ lines = my_file.readlines()
 for line in lines:
   l.append(line.strip())
 
-print(l)
+reg = {}
+reg['a'] = 0
+reg['b'] = 0
+reg['c'] = 0
+reg['d'] = 0
 
+i = 0
+while i < len(l):
+  s = l[i]
+  arr = s.split(' ')
+  cmd = arr[0]
+  arg1 = arr[1]
+  arg2 = ''
+  val = 0
+  if len(arr) == 3:
+    arg2 = arr[2]
+
+  if cmd == 'cpy':
+    if arg1.isalpha():
+      val = reg[arg1]
+    else:
+      val = int(arg1)
+    reg[arg2] = val
+    i += 1
+  elif cmd == 'inc':
+    reg[arg1] += 1
+    i += 1
+  elif cmd == 'dec':
+    reg[arg1] -= 1
+    i += 1
+  elif cmd == 'jnz':
+    if arg1.isalpha():
+      val = reg[arg1]
+    else:
+      val = int(arg1)
+    if val != 0:
+      i = i + int(arg2)
+    else:
+      i += 1
+
+print(reg)
 
 print('')
 end_secs = time.time()
