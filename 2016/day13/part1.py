64a466c51b8bde363e3a0529d6741cdff43d0264
Edwin Torres <CoachEd@gmail.com>
2022-01-06 14:34:50 -0500

    initial day 13

---
+++ 2016/day13/part1.py
@@ -0,0 +1,23 @@
+"""
+AoC
+"""
+import time
+import sys
+import math
+from copy import copy, deepcopy
+
+start_secs = time.time()
+print('')
+
+# read in input file
+l=[]
+my_file = open("inp.txt", "r", encoding='utf-8')
+lines = my_file.readlines()
+for line in lines:
+  l.append(line.strip())
+
+print(l)
+
+print('')
+end_secs = time.time()
+print('--- ' + str(end_secs-start_secs) + ' secs ---')
