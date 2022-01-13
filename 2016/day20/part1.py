3bf0fdbcbdd383c7402bc5403d72f69424958396
Edwin Torres <CoachEd@gmail.com>
2022-01-13 16:01:21 -0500

    part1

--- 2016/day20/part1.py
+++ 2016/day20/part1.py
@@ -2,6 +2,7 @@
 AoC
 """
 import time
+import sys
 
 start_secs = time.time()
 print('')
@@ -13,7 +14,32 @@ lines = my_file.readlines()
 for line in lines:
   l.append(line.strip())
 
-print(l)
+ranges = {}
+low = sys.maxsize
+arr = [-1 for i in range(len(l))]
+for i in range(len(l)):
+  s = l[i]
+  arr2 = s.split('-')
+  start = int(arr2[0])
+  end = int(arr2[1])
+  arr[i] = start
+  ranges[start] = end
+
+arr.sort()
+
+pos = 0
+for i in range(len(arr)-1):
+  start = arr[i]
+  end = ranges[start]
+  next_start = arr[i+1]
+  if pos >= start and pos <= end:
+    pos = end + 1
+    continue
+  if pos < next_start:
+    print(pos)
+    break
+  pos += 1
+
 
 
 print('')
