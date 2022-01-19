480536ed78b18199dcecb573fd39469f905ece0e
Edwin Torres <CoachEd@gmail.com>
2022-01-19 13:34:53 -0500

    latest

---
+++ 2016/day22/part2.py
@@ -0,0 +1,65 @@
+"""
+AoC
+"""
+import time
+
+start_secs = time.time()
+print('')
+
+# read in input file
+l=[]
+my_file = open("inp.txt", "r", encoding='utf-8')
+lines = my_file.readlines()
+for line in lines:
+  len_line = 0
+  line = line.strip()
+  while len_line != len(line):
+    len_line = len(line)
+    line = line.replace('  ',' ')
+
+  l.append(line)
+
+# Filesystem              Size  Used  Avail  Use%
+# /dev/grid/node-x29-y27   92T   72T    20T   78%
+nodes = {}
+max_x = -1
+max_y = -1
+node_num = 0
+for i in range(2,len(l)):
+  arr = l[i].split(' ')
+  arr2 = arr[0].split('-')
+  x = int(arr2[1][1:])
+  y = int(arr2[2][1:])
+  if x > max_x:
+    max_x = x
+  if y > max_y:
+    max_y = y
+  
+  size = arr[1]
+  i = size.find('T')
+  size = int(size[0:i])
+
+  used = arr[2]
+  i = used.find('T')
+  used = int(used[0:i])
+
+  avail = arr[3]
+  i = avail.find('T')
+  avail = int(avail[0:i])
+
+  pct = arr[4]
+  i = pct.find('%')
+  pct = int(pct[0:i])  
+
+  node_name = str(x)+':'+str(y)
+  nodes[node_num] = [x,y,size,used,avail,pct]
+  node_num += 1
+
+print('max_x: ' + str(max_x))
+print('max_y: ' + str(max_y))
+
+
+
+print('')
+end_secs = time.time()
+print('--- ' + str(end_secs-start_secs) + ' secs ---')
