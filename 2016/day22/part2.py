f36eaac276fcd6530bd2a41c7bfc3228981d17f5
Edwin Torres <CoachEd@gmail.com>
2022-01-19 17:08:57 -0500

    latest

--- 2016/day22/part2.py
+++ 2016/day22/part2.py
@@ -2,6 +2,61 @@
 AoC
 """
 import time
+import copy
+
+def find_xy(x,y,x2,y2,seen,arr,steps,nodes):
+  nodes = copy.deepcopy(nodes)
+  arr = copy.deepcopy(arr)
+  # [x,y,size,used,avail,pct]
+  key = str(x)+':'+str(y)
+  node1 = nodes[key]
+  size = node1[2]
+  used = node1[3]
+  if used != 0:
+    print('error-must start from 000 used: ' + (x,y))
+    return
+
+  (tx,ty) = (x,y-0)
+  (bx,by) = (x,y+1)
+  (rx,ry) = (x+1,y)
+  (lx,ly) = (x-1,y)
+  good_points = []
+  if has_xy(tx,ty,arr):
+    good_points.append((tx,ty))
+  if has_xy(bx,by,arr):
+    good_points.append((bx,by))
+  if has_xy(rx,ry,arr):
+    good_points.append((rx,ry))
+  if has_xy(lx,ly,arr):
+    good_points.append((lx,ly))    
+  if (x,y) in seen:
+    # no back-tracking
+    return
+  seen.append( (x,y) )
+  # next to the destination?
+  if (x2,y2) in good_points:
+    print(steps)
+    return
+  if len(good_points) > 0:
+    steps += 1
+
+    for p in good_points:
+      (x1,y1) = p
+      key2 = str(x1)+':'+str(y1)
+      node2 = nodes[key2]
+      used2 = node2[3]
+      if size >= used2:
+        # switch
+        # [x,y,size,used,avail,pct]
+        node1[3] = node2[3]
+        node2[3] = 0
+        arr9 = copy.deepcopy(arr)
+        find_xy(x1,y1,x2,y2,seen,arr9,steps,nodes)
+
+def has_xy(x,y,arr):
+  if x < 0 or y < 0 or x >= len(arr[0]) or y >= len(arr):
+    return False
+  return True
 
 def print_mem(arr,nodes):
   # [x,y,size,used,avail,pct]
@@ -83,8 +138,60 @@ for k,v in nodes.items():
   mem_location[0] = v[3] # used
   mem_location[1] = v[2] # size
 
-#print_mem(arr,nodes)
 
+
+
+find_xy(28,34,34,0,[],arr,0,nodes)
+
+
+# remove nodes that cannot move
+# found none?
+
+"""
+for y in range(len(arr)):
+  for x in range(len(arr[y])):
+    
+    # top
+    x1 = x
+    y1 = y-1
+    key1 = str(x1) +':'+str(y1)
+    key = str(x) +':'+str(y)
+    can_move = False
+    if has_xy(x1,y1,arr) and nodes[key1][2] >= nodes[key][3]:
+      continue
+
+    # bottom
+    x1 = x
+    y1 = y+1
+    key1 = str(x1) +':'+str(y1)
+    key = str(x) +':'+str(y)
+    can_move = False
+    if has_xy(x1,y1,arr) and nodes[key1][2] >= nodes[key][3]:
+      continue    
+    
+    # right
+    x1 = x+1
+    y1 = y
+    key1 = str(x1) +':'+str(y1)
+    key = str(x) +':'+str(y)
+    can_move = False
+    if has_xy(x1,y1,arr) and nodes[key1][2] >= nodes[key][3]:
+      continue  
+
+    # left
+    x1 = x-1
+    y1 = y
+    key1 = str(x1) +':'+str(y1)
+    key = str(x) +':'+str(y)
+    can_move = False
+    if has_xy(x1,y1,arr) and nodes[key1][2] >= nodes[key][3]:
+      continue      
+
+    # if we make it here, this node cannot move. remove it.
+    arr[y][x] = '   .   '
+
+print_mem(arr,nodes)
+"""
 
 
 print('')
