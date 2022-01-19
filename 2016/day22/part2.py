3172782c4eb5e2ab6ba1c529cfb9690b063e2bc9
Edwin Torres <CoachEd@gmail.com>
2022-01-19 17:35:59 -0500

    latest

--- 2016/day22/part2.py
+++ 2016/day22/part2.py
@@ -4,70 +4,65 @@ AoC
 import time
 import copy
 
-def find_xy(x,y,x2,y2,seen,arr,steps,nodes):
-  nodes = copy.deepcopy(nodes)
+def find_xy(x,y,x2,y2,seen,arr,steps):
+  seen = copy.deepcopy(seen)
+  seen.append( (x,y) )
   arr = copy.deepcopy(arr)
   # [x,y,size,used,avail,pct]
-  key = str(x)+':'+str(y)
-  node1 = nodes[key]
-  size = node1[2]
-  used = node1[3]
+  node1 = arr[y][x]
+  used = node1[0]
+  size = node1[1]  
   if used != 0:
     print('error-must start from 000 used: ' + (x,y))
     return
-
-  (tx,ty) = (x,y-0)
+  (tx,ty) = (x,y-1)
   (bx,by) = (x,y+1)
   (rx,ry) = (x+1,y)
   (lx,ly) = (x-1,y)
   good_points = []
-  if has_xy(tx,ty,arr):
+  if has_xy(tx,ty,arr) and not (tx,ty) in seen:
     good_points.append((tx,ty))
-  if has_xy(bx,by,arr):
+  if has_xy(bx,by,arr) and not (bx,by) in seen:
     good_points.append((bx,by))
-  if has_xy(rx,ry,arr):
+  if has_xy(rx,ry,arr) and not (rx,ry) in seen:
     good_points.append((rx,ry))
-  if has_xy(lx,ly,arr):
-    good_points.append((lx,ly))    
-  if (x,y) in seen:
-    # no back-tracking
-    return
-  seen.append( (x,y) )
-  # next to the destination?
-  if (x2,y2) in good_points:
-    print(steps)
-    return
-  if len(good_points) > 0:
-    steps += 1
+  if has_xy(lx,ly,arr) and not (lx,ly) in seen:
+    good_points.append((lx,ly))
 
-    for p in good_points:
-      (x1,y1) = p
-      key2 = str(x1)+':'+str(y1)
-      node2 = nodes[key2]
-      used2 = node2[3]
-      if size >= used2:
-        # switch
-        # [x,y,size,used,avail,pct]
-        node1[3] = node2[3]
-        node2[3] = 0
-        arr9 = copy.deepcopy(arr)
-        find_xy(x1,y1,x2,y2,seen,arr9,steps,nodes)
+  if len(good_points) == 0:
+    # no moves
+    return
+
+  for p in good_points:
+    (x1,y1) = p
+    node2 = arr[y1][x1]
+    used2 = node2[0]
+
+    # next to destination
+    if x2 == x1 and y2 == y1:
+      print(steps)
+      return
+
+    if size >= used2:
+      # switch
+      steps += 1
+      # [x,y,size,used,avail,pct]
+      node1[0] = node2[0]
+      node2[0] = 0
+      find_xy(x1,y1,x2,y2,seen,arr,steps)
 
 def has_xy(x,y,arr):
   if x < 0 or y < 0 or x >= len(arr[0]) or y >= len(arr):
     return False
   return True
 
-def print_mem(arr,nodes):
-  # [x,y,size,used,avail,pct]
+def print_mem(arr):
   s = ''
   for r in range(len(arr)):
     for c in range(len(arr[r])):
       e = arr[r][c]
       if e[0] != -1:
-        node_name = str(c)+':'+str(r)
-        node = nodes[node_name]
-        mem_info = "{:03d}".format(node[3]) + '/' + "{:03d}".format(node[2]) + ' '
+        mem_info = "{:03d}".format(e[0]) + '/' + "{:03d}".format(e[1]) + ' '
         s += mem_info
       else:
         s += '.'
@@ -137,11 +132,10 @@ for k,v in nodes.items():
   mem_location = arr[v[1]][v[0]] # y,x
   mem_location[0] = v[3] # used
   mem_location[1] = v[2] # size
+print_mem(arr)
 
-
-
-
-find_xy(28,34,34,0,[],arr,0,nodes)
+# NOT FINISHING???
+# find_xy(28,34,34,0,[],arr,0)
 
 
 # remove nodes that cannot move
