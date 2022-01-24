f1718ab5c5d0a2c4f07fcf6d852f4dc828a0f91d
Edwin Torres <CoachEd@gmail.com>
2022-01-24 11:44:23 -0500

    latest

--- 2016/day23/part1.py
+++ 2016/day23/part1.py
@@ -17,7 +17,7 @@ for line in lines:
   l.append(line.strip())
 
 reg = {}
-reg['a'] = 0
+reg['a'] = 7  # INPUT
 reg['b'] = 0
 reg['c'] = 0
 reg['d'] = 0
@@ -30,9 +30,12 @@ while i < len(l):
   arg1 = arr[1]
   arg2 = ''
   val = 0
+  val2 = 0
+
+  #print('CMD: ' + cmd + '   ' + str(reg) + ' :   ' + str(l))  # TEST
+
   if len(arr) == 3:
     arg2 = arr[2]
-
   if cmd == 'cpy':
     if arg1.isalpha():
       val = reg[arg1]
@@ -51,12 +54,43 @@ while i < len(l):
       val = reg[arg1]
     else:
       val = int(arg1)
+
+    if arg2.isalpha():
+      val2 = reg[arg2]
+    else:
+      val2 = int(arg2)
+  
     if val != 0:
-      i = i + int(arg2)
+      i = i + int(val2)
     else:
       i += 1
+  elif cmd == 'tgl':
+    if arg1.isalpha():
+      val = reg[arg1]
+    else:
+      val = int(arg1)
 
-print(reg)
+    if (val+i) >= 0 and (val+i) < len(l):
+      instr = l[i+val]
+      cmd1 = instr[0:3]
+      cmd2 = ''
+      arr2 = instr.split(' ')
+      if len(arr2) == 2: 
+        if cmd1 == 'inc':
+          cmd2 = 'dec'
+        else:
+          cmd2 = 'inc'
+      else:
+        if cmd1 == 'jnz':
+          cmd2 = 'cpy'
+        else:
+          cmd2 = 'jnz'
+      l[i+val] = cmd2 + instr[3:]
+    i += 1
+
+  #print(cmd + ' - ' + str(reg) + ' ' + str(l) + '\n') # TEST
+
+print(reg['a'])
 
 print('')
 end_secs = time.time()
