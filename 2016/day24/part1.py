685728497ac7f03fd11bb1ffac31f40c85d0069a
Edwin Torres <CoachEd@gmail.com>
2022-01-25 15:04:10 -0500

    latest

--- 2016/day24/part1.py
+++ 2016/day24/part1.py
@@ -5,6 +5,7 @@ import time
 import sys
 import copy
 
+found = False
 def valid(y,x,arr):
   if y < 0 or x < 0 or y >= len(arr) or x >= len(arr[0]):
     return False
@@ -13,6 +14,9 @@ def valid(y,x,arr):
   return True
 
 def find_target(n,y,x,steps,arr,seen):
+  global found
+  if found:
+    return
   (ty,tx,by,bx,ry,rx,ly,lx) = (y-1,x,y+1,x,y,x+1,y,x-1)
   seen.add((y,x))
   moves = []
@@ -25,13 +29,15 @@ def find_target(n,y,x,steps,arr,seen):
   if valid(ly,lx,arr):
     moves.append([ly,lx])
 
+  new_steps = steps
   for move in moves:
     (y1,x1) = (move[0],move[1])
     if (y1,x1) in seen:
       continue
+    new_steps = steps + 1
     if arr[y1][x1] == n:
-      steps += 1
-      print(steps)
+      found = True
+      print('steps: ' + str(new_steps) +   '   0 y,x: ' + str((y1,x1)))
       return
     else:
       arr1 = copy.deepcopy(arr)
@@ -40,8 +46,7 @@ def find_target(n,y,x,steps,arr,seen):
       arr1[y1][x1] = my_char
       seen1 = seen.copy()
       seen1.add( (y1,x1) )
-      steps += 1
-      find_target(n,y1,x1,steps,arr1,seen1)
+      find_target(n,y1,x1,new_steps,arr1,seen1)
 
 def print_map(arr):
   s = ''
@@ -56,7 +61,7 @@ print('')
 
 # read in input file
 l=[]
-my_file = open("inp_small.txt", "r", encoding='utf-8')
+my_file = open("inp.txt", "r", encoding='utf-8')
 lines = my_file.readlines()
 for line in lines:
   l.append(line.strip())
@@ -76,8 +81,21 @@ for y in range(rows):
     arr[y][x] = c
 
 
+# 0 to 4
 (y,x) = (targets[0][0],targets[0][1])
 find_target(4,y,x,0,arr,set())
+# OUTPUT: steps: 90   0 y,x: (17, 165)
+
+# 0 to 5
+"""
+arr[y][x] = '.'
+targets[0][0] = 17
+targets[0][1] = 165
+(y,x) = (targets[0][0],targets[0][1])
+arr[y][x] = 0
+find_target(5,y,x,0,arr,set())
+# OUTPUT: ?
+"""
 
 
 print('')
