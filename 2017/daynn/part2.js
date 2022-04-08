34b794bd62983343f319688d39ab70baff7905b7
Edwin Torres <CoachEd@gmail.com>
2022-04-08 10:06:38 -0400

    updates

---
+++ 2017/daynn/part2.js
@@ -0,0 +1,15 @@
+const ac = require('../../js/aocCommon');
+
+// main - part 2
+
+const go = async () => {
+  const startTime = process.hrtime();
+
+  const sarr = await ac.getInputArray('inp.txt');
+  console.log(sarr);
+
+  const endTime = process.hrtime();
+  console.log(`elapsed time: ${((endTime[0] / 1000000000 + endTime[1]) - (startTime[0] / 1000000000 + startTime[1])) / 1000000000}`);
+};
+
+go();
