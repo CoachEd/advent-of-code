08b86af6d4966f9dda2c3a38c8dbc424de11b548
Edwin Torres <CoachEd@gmail.com>
2022-04-25 09:32:14 -0400

    fix

--- 2016/day01/part1.py
+++ 2016/day01/part1.py
@@ -14,8 +14,8 @@ for line in lines:
 # TODO
 arr = l[0].replace(' ','').split(',')
 facing = 90 # N-90, E-180, S-270, W-0
-row = 0
-col = 0
+y = 0
+x = 0
 for d in arr:
   direction = d[0]
   distance = int(d[1:])
@@ -28,17 +28,16 @@ for d in arr:
   elif facing > 270:
     facing = 0
   if facing == 90:
-    row = row + distance
+    y = y + distance
   elif facing == 180:
-    col = col + distance
+    x = x + distance
   elif facing == 270:
-    row = row - distance
+    y = y - distance
   elif facing == 0:
-    col = col - distance
+    x = x - distance
   
-print('row: ' + str(row))
-print('col: ' + str(col))
-print('blocks: ' + str(row + col))
+print('x,y: ' + str(x) + ',' + str(y))
+print('blocks: ' + str(y + x))
 
 print('')
 end_secs = time.time()
