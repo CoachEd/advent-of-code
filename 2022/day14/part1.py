2dcdd8275e29d65b0cd8c1a464eea9ae45862622
Edwin Torres <CoachEd@gmail.com>
2022-12-14 16:42:08 -0500

    latest

--- 2022/day14/part1.py
+++ 2022/day14/part1.py
@@ -4,10 +4,24 @@ from copy import copy, deepcopy
 start_secs = time.time()
 print('')
 
-def printCave(a):
+def countSand(a):
   global min_x, min_y, max_x, max_y
+  count = 0
+  start_y = min_y - 4
+  end_y = max_y + 3
+  start_x = min_x - 3
+  end_x = max_x + 3
+
+  for y in range(start_y, end_y):
+    for x in range(start_x, end_x):
+      if a[y][x] == 'O':
+        count += 1
+  return count
+
+def printCave(a):
+  global min_x, min_y, max_x, max_y, found_void
   s = ''
-  start_y = min_y - 3
+  start_y = min_y - 4
   end_y = max_y + 3
   start_x = min_x - 3
   end_x = max_x + 3
@@ -18,6 +32,46 @@ def printCave(a):
     s += '\n'
   print(s)
 
+def dropSand(a):
+  global sand_x, sand_y, min_x, min_y, max_x, max_y
+  x = sand_x
+  y = sand_y
+  while True:
+
+    if a[y][x] == 'O':
+      if y > max_y or x < min_x or x > max_x:
+        # endless void
+        a[y][x] = '.'
+        found_void = True
+        return
+
+    if a[y+1][x] != '.' and a[y+1][x-1] != '.' and a[y+1][x+1] != '.':
+      return True
+    
+    if a[y+1][x] != '.' and a[y+1][x-1] == '.':
+      # move diagonally down left
+      a[y][x] = '.'
+      a[y+1][x-1] = 'O'
+      y = y+1
+      x = x-1
+    elif a[y+1][x] != '.' and a[y+1][x+1] == '.':
+      # move diagonally down right
+      a[y][x] = '.'
+      a[y+1][x+1] = 'O'
+      y = y+1
+      x = x+1     
+    elif a[y+1][x] == '.':
+      # move down
+      a[y][x] = '.'
+      a[y+1][x] = 'O'
+      y = y+1
+
+    #printCave(a)
+
+
+
+# MAIN
+
 # read in input file
 l=[]
 my_file = open("inp.txt", "r", encoding='utf-8')
@@ -25,6 +79,9 @@ lines = my_file.readlines()
 for line in lines:
   l.append(line.strip())
 
+found_void = False
+sand_x = 500
+sand_y = 0
 max_x = -1
 max_y = -1
 min_x = sys.maxsize
@@ -47,6 +104,7 @@ for s in l:
 cols = max_x + 5
 rows = max_y + 5
 cave = [ ['.' for x in range(cols)] for y in range(rows) ]
+cave[sand_y][sand_x] = '+'
 
 for s in l:
   arr = s.split(' -> ')
@@ -79,7 +137,14 @@ for s in l:
         cave[y][x0] = '#'
 
 
-printCave(cave)
+#printCave(cave)
+while not found_void:
+  dropSand(cave)
+
+#printCave(cave)
+
+print( countSand(cave) )
+# 699 WRONG TOO LOW
 
 print('')
 end_secs = time.time()
