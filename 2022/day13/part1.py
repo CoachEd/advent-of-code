daab8415f01b4d9b5486572f25c8ccbd85f56ccf
Edwin Torres <CoachEd@gmail.com>
2022-12-14 11:37:42 -0500

    good part1?

--- 2022/day13/part1.py
+++ 2022/day13/part1.py
@@ -21,31 +21,54 @@ def isList(x):
 
 def isOrder(left,right):
   # two lists as input parameters
+  #print(left)
+  #print(right)
+  #print()
 
+  # if both are integers
+  if isInt(left) and isInt(right):  
+    if left < right:
+      return True
+    elif left > right:
+      return False
+    else:
+      return 2
+
+  # if one is a list and one is an integer, make the integer a list
+  if isList(left) and isInt(right):
+    right = [right]
+  elif isInt(left) and isList(right):
+    left = [left]
+
+  # we now have TWO lists
+
+  # run out of items?
+  if len(left) == 0 and len(right) > 0:
+    return True
+  if len(left) > 0 and len(right) == 0:
+    return False
+
+  # loop through first elements and compare them
+
+  # special case for empty lists. NEEDED?
+  if len(left) == 0 and len(right) == 0:
+    left = [0]
+    right = [0]
+
+  rval = 2
   for i in range(len(left)):
-    if i >= len(right):
-      return False  # right ran out
-    l = left[i]
-    r = right[i]
+    if (i >= len(right)):
+      # right ran out
+      return False
+    rval = isOrder(left[i], right[i])
+    if rval != 2:
+      return rval
 
-    # l and r are integers
-    if isInt(l) and isInt(r):  
-      if l < r:
-        return True
-      elif l > r:
-        return False
-    elif isList(l) and isList(r):
-      # TODO: l and r are both lists
-      pass
-    elif isList(l) and isInt(r):
-      # TODO: l is a list and r is an integer
-      pass
-    elif isInt(l) and isList(r):
-      # TODO: l is an integer and r is a list
-      pass
+  # run out of items?
+  if len(left) < len(right):
+    return True
 
-
-
+  return rval
     
 # main
 index = 1
@@ -53,11 +76,12 @@ tot = 0
 for i in range(0, len(l), +3):
   left = literal_eval(l[i])
   right = literal_eval(l[i+1])
-  if isOrder(left, right):
-    print('IN ORDER')
+  rval = isOrder(left, right)
+  if rval == True:
+    print('%s passed' % (index,))
     tot += index
   else:
-    print('NOT IN ORDER')
+    print('%s failed' % (index,))
   index += 1
 
 print()
