e82e2e660952b613894dfb1eab8705d64bb08cb7
Edwin Torres <CoachEd@gmail.com>
2022-12-16 14:07:11 -0500

    part1 sample input

--- 2022/day15/part1.py
+++ 2022/day15/part1.py
@@ -4,54 +4,6 @@ from copy import copy, deepcopy
 start_secs = time.time()
 print('')
 
-def getArea(sx,sy,bx,by):
-  dist = abs((sx-bx)) + abs((sy-by))
-  points = []
-
-  # float('-inf') smallest number in Python
-  # float('+inf') largest number in Python
-
-  min_x = sx - dist
-  max_x = sx + dist
-  min_y = sy - dist
-  max_y = sy + dist
-
-  # add points left and right of sensor
-  lx = sx - 1
-  rx = sx + 1
-  for i in range(dist):
-    points.append((lx,sy))
-    points.append((rx,sy))
-    lx -= 1
-    rx += 1
-
-  # add points top and bottom of sensor
-  uy = sy - 1
-  dy = sy + 1
-  d2 = dist - 1
-  for i in range(dist):
-    points.append((sx,uy))
-    points.append((sx,dy))
-
-    lx = sx - 1
-    rx = sx + 1
-    for i in range(d2):
-      points.append((lx,uy))
-      points.append((rx,uy))
-      points.append((lx,dy))
-      points.append((rx,dy))      
-      lx -= 1
-      rx += 1
-    uy -= 1
-    dy += 1
-    d2 -= 1
-
-  # remove beacon from the list
-  points.remove((bx,by))
-  return (points, min_x, max_x, min_y, max_y)
-
-
-
 # read in input file
 l=[]
 my_file = open("inp.txt", "r", encoding='utf-8')
@@ -75,10 +27,9 @@ for s in l:
   beacons.append((bx,by))
 
 # for each sensor, calculate its areas
-positions = 0
-seen = []
 row = 10 # TODO: INPUT
-points = set()
+#row = 2000000 # TODO: INPUT
+xs = set()
 for i in range(len(sensors)):
   (sx,sy) = sensors[i]
   (bx,by) = beacons[i]
@@ -96,21 +47,23 @@ for i in range(len(sensors)):
 
     start_x = lx
     end_x = rx
-    count = abs(end_x - start_x) + 1
-    
+    for i in range(start_x, end_x + 1):
+      xs.add(i)
+
     # remove sensors
     for (x,y) in sensors:
       if x >= start_x and x <= end_x and y == row:
-        count -= 1
-    
+        if x in xs:
+          xs.remove(x)
+
     # remove beacons
     for (x,y) in beacons:
       if x >= start_x and x <= end_x and y == row:
-        count -= 1
+        if x in xs:
+          xs.remove(x)
 
-    positions += count
 
-print(positions)
+print(len(xs))
 # 6262636 TOO HIGH
 # 9584037
 
