5d8828c6bc0bc5d13242a54847d67b912d190dbc
Edwin Torres <CoachEd@gmail.com>
2023-02-15 15:18:08 -0500

    latest

--- 2017/day10/part2.py
+++ 2017/day10/part2.py
@@ -40,23 +40,38 @@ lines = my_file.readlines()
 for line in lines:
   l.append(line.strip())
 
-l = [ int(n) for n in l[0].split(',') ]
+l1 = []
+l2 = [17, 31, 73, 47, 23]
+for c in l[0]:
+  l1.append(ord(c))
+l = l1 + l2
+l0 = l.copy()
 
 LIST_SIZE = 256
 arr = [ n for n in range(LIST_SIZE)]
+
 curr = 0
 skip = 0
-sz = len(arr)
-for len1 in l:
-  #print('skip = ' + str(skip) + '  ' + '  curr = ' + str(curr) + '  len = ' + str(len1))
-  #print(arr)
-  reverse(arr,curr,len1)
-  curr = (curr + len1 + skip) % sz
-  skip += 1
-  #print(arr)
-  #print()
+for i in range(64):
+  sz = len(arr)
+  for len1 in l:
+    reverse(arr,curr,len1)
+    curr = (curr + len1 + skip) % sz
+    skip += 1
 
-print(arr[0] * arr[1])
+l3 = []
+for i in range(0, 256, 16):
+  n = arr[i]
+  for j in range(i+1, i+16, 1):
+    n = n ^ arr[j]
+  l3.append(n)
+
+s = ''
+for n in l3:
+  hx = "0x{:02x}".format(n)
+  s += hx[2:]
+
+print(s)
 
 print('')
 end_secs = time.time()
