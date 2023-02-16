a585865b7f9c815f65b3f8ddfc5e5a9be6070d21
Edwin Torres <CoachEd@gmail.com>
2023-02-16 10:36:07 -0500

    part1

--- 2017/day11/part1.py
+++ 2017/day11/part1.py
@@ -12,8 +12,59 @@ my_file = open("inp.txt", "r", encoding='utf-8')
 lines = my_file.readlines()
 for line in lines:
   l.append(line.strip())
-print(l)
 
+s = l[0] + ','
+directions = ['nw,', 'n,', 'ne,', 'sw,', 's,', 'se,']
+
+# nw n ne sw s se
+while True:
+  len1 = len(s)
+  (d1, d2) = ('n,', 's,')
+  if d1 in s and d2 in s:
+    s = s.replace(d1,'',1)
+    s = s.replace(d2,'',1)
+
+  (d1, d2) = ('nw,', 'se,')
+  if d1 in s and d2 in s:
+    s = s.replace(d1,'',1)
+    s = s.replace(d2,'',1)
+
+  (d1, d2) = ('ne,', 'sw,')
+  if d1 in s and d2 in s:
+    s = s.replace(d1,'',1)
+    s = s.replace(d2,'',1)    
+
+  len2 = len(s)
+  if len1 == len2:
+    break
+
+steps = 0
+d1 = {}
+for d in directions:
+  n = s.count(d)
+  d1[d] = n
+
+
+for c in d1:
+  print(c + ': ' + str(d1[c]))
+"""
+nw,: 0
+n,: 0
+ne,: 256
+sw,: 0
+s,: 43
+se,: 440
+"""
+
+# s: 43 is irrelevant
+# count is...
+
+print(d1['ne,'] + d1['se,'])
+
+
+# 526  TOO LOW
+# 739  TOO HIGH
+# 923  TOO HIGH
 
 print('')
 end_secs = time.time()
