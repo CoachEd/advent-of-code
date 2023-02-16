1271b4d0eef922642829c04453c5507b7700448e
Edwin Torres <CoachEd@gmail.com>
2023-02-16 12:46:48 -0500

    latest

--- 2017/day11/part2.py
+++ 2017/day11/part2.py
@@ -12,8 +12,36 @@ my_file = open("inp.txt", "r", encoding='utf-8')
 lines = my_file.readlines()
 for line in lines:
   l.append(line.strip())
-print(l)
 
+s = l[0]
+arr = s.split(',')
+
+x_pos = 0
+y_pos = 0
+max_distance = 0
+for d in arr:
+  if d == 'n':
+    y_pos += 1
+  elif d == 's':
+    y_pos -= 1
+  elif d == 'ne':
+    x_pos += 1
+    y_pos += 1
+  elif d == 'nw':
+    x_pos -= 1
+    y_pos += 1    
+  elif d == 'se':
+    x_pos += 1
+    y_pos -= 1
+  elif d == 'sw':
+    x_pos -= 1
+    y_pos -= 1  
+
+  temp_distance = max(abs(y_pos), abs(x_pos))
+  if temp_distance > max_distance:
+    max_distance = temp_distance
+
+print(max_distance)
 
 print('')
 end_secs = time.time()
