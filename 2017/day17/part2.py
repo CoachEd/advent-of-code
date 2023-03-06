48523eb23f461426d9433f2dcdc3b080b652a08d
Edwin Torres <CoachEd@gmail.com>
2023-03-06 16:09:17 -0500

    does not scale to 50000000

--- 2017/day17/part2.py
+++ 2017/day17/part2.py
@@ -4,16 +4,38 @@ from copy import copy, deepcopy
 start_secs = time.time()
 print('')
 
+def addOne(n):
+  global arr
+  global steps
+  global pos
+  if len(arr) == 1:
+    arr.append(n)
+    pos = 1
+  else:
+    rem = steps % len(arr)
+    new_pos = pos + rem + 1
+    if new_pos < len(arr):
+      pos = new_pos
+    else:
+      pos = new_pos - len(arr)
+    arr.insert(pos, n)
 
-# SOLUTION
-# read in input file
-l=[]
-my_file = open("inp.txt", "r", encoding='utf-8')
-lines = my_file.readlines()
-for line in lines:
-  l.append(line.strip())
-print(l)
+steps = 359 # real
+#steps = 3 # sample
+pos = 0
+arr = [0]
 
+insertions = 2017
+for i in range(1, insertions + 1):
+  addOne(i)
+
+i = arr.index(2017)
+if i == len(arr) - 1:
+  i = 0
+else:
+  i += 1
+
+print(arr[i])
 
 print('')
 end_secs = time.time()
