1a9904f481013a7e2884e43e77910127590273b1
Edwin Torres <CoachEd@gmail.com>
2023-03-14 15:59:53 -0400

    unoptimized; does not scale

--- 2017/day23/part2.py
+++ 2017/day23/part2.py
@@ -4,17 +4,56 @@ from copy import copy, deepcopy
 start_secs = time.time()
 print('')
 
+def exec_command(s):
+  global registers, iindex
+  a = s.split()
+  cmd = a[0]
+  X = a[1]
+  Y = a[2]
+  if cmd == 'jnz':
+    if X.isalpha():
+      X = int(registers[X])
+    else:
+      X = int(X)
+    Y = int(Y)
+    if X != 0:
+      iindex += Y
+      return
+  else:
+    if Y.isalpha():
+      Y = registers[Y]
+    else:
+      Y = int(Y)
+    if cmd == 'set':
+      registers[X] = Y
+    elif cmd == 'sub':
+      registers[X] -= Y
+    elif cmd == 'mul':
+      registers[X] *= Y
+  iindex += 1
 
-# SOLUTION
+iindex = 0
+registers = dict()
+for c in 'abcdefgh':
+  registers[c] = 0
+
+# debug switch
+registers['a'] = 1
+
 # read in input file
 l=[]
 my_file = open("inp.txt", "r", encoding='utf-8')
 lines = my_file.readlines()
 for line in lines:
   l.append(line.strip())
-print(l)
 
+num_commands = len(l)
+while iindex < num_commands:
+  s = l[iindex]
+  exec_command(s)
 
+print()
+print(registers['h'])
 print('')
 end_secs = time.time()
 print('--- ' + str(end_secs-start_secs) + ' secs ---')
