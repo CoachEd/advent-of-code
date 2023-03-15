44f8bd9e8566b394fef35eea036a8b43a0ec0e0f
Edwin Torres <CoachEd@gmail.com>
2023-03-15 10:08:35 -0400

    add and nop cmds added

--- 2017/day23/part2.py
+++ 2017/day23/part2.py
@@ -6,6 +6,11 @@ print('')
 
 def exec_command(s):
   global registers, iindex
+
+  if s == 'nop':
+    iindex += 1
+    return
+
   a = s.split()
   cmd = a[0]
   X = a[1]
@@ -28,6 +33,8 @@ def exec_command(s):
       registers[X] = Y
     elif cmd == 'sub':
       registers[X] -= Y
+    elif cmd == 'add':
+      registers[X] += Y      
     elif cmd == 'mul':
       registers[X] *= Y
   iindex += 1
@@ -51,7 +58,17 @@ for line in lines:
 num_commands = len(l)
 while iindex < num_commands:
   s = l[iindex]
+
+  #if iindex == 11:
+  #  print(str(registers) + '  set g d')
+  
+  #print(s)
+  
   exec_command(s)
+
+  #if iindex == 19:
+  #  print(str(registers) + '  jnz g -8')
+
 
 print()
 print(registers['h'])
