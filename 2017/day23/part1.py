fba918ad8773d3a905272b721bb4df25bf3bfe9c
Edwin Torres <CoachEd@gmail.com>
2023-11-28 11:40:35 -0500

    latest try

--- 2017/day23/part1.py
+++ 2017/day23/part1.py
@@ -4,57 +4,55 @@ from copy import copy, deepcopy
 start_secs = time.time()
 print('')
 
-def exec_command(s):
-  global registers, iindex, num_mul
-  a = s.split()
-  cmd = a[0]
-  if cmd == 'nop':
-    iindex += 1
-    return  
-  X = a[1]
-  Y = a[2]
-  if cmd == 'jnz':
-    if X.isalpha():
-      X = int(registers[X])
-    else:
-      X = int(X)
-    Y = int(Y)
-    if X != 0:
-      iindex += Y
-      return
+registers = {}
+iindex = 0
+mul_count = 0
+
+def init_registers():
+  global registers
+  (registers['a'], registers['b'], registers['c'], registers['d'], registers['e'], registers['f'], registers['g'], registers['h']) = (0,0,0,0,0,0,0,0)
+
+def valueOf(z):
+  global registers
+  if z.isalpha():
+    return registers[z]
   else:
-    if Y.isalpha():
-      Y = registers[Y]
-    else:
-      Y = int(Y)
-    if cmd == 'set':
-      registers[X] = Y
-    elif cmd == 'sub':
-      registers[X] -= Y
-    elif cmd == 'mul':
-      registers[X] *= Y
-      num_mul += 1
+    return int(z)
+
+def exec_cmd_str(s):
+  global iindex, registers, mul_count
+  arr = s.split()
+  cmd = arr[0]
+  arg1 = arr[1]
+  arg2 = arr[2]
+  if cmd == 'set':
+    registers[arg1] = valueOf(arg2)
+  elif cmd == 'sub':
+    registers[arg1] = registers[arg1] - valueOf(arg2)
+  elif cmd == 'mul':
+    registers[arg1] = registers[arg1] * valueOf(arg2)
+    mul_count += 1
+  elif cmd == 'jnz':
+    if valueOf(arg1) != 0:
+      iindex += valueOf(arg2)
+      return
   iindex += 1
 
-iindex = 0
-num_mul = 0
-registers = dict()
-for c in 'abcdefgh':
-  registers[c] = 0
+# main
+init_registers()
 
-# read in input file
+# read input file
 l=[]
 my_file = open("inp.txt", "r", encoding='utf-8')
 lines = my_file.readlines()
 for line in lines:
   l.append(line.strip())
-
 num_commands = len(l)
 while iindex < num_commands:
   s = l[iindex]
-  exec_command(s)
+  exec_cmd_str(s)
 
-print(num_mul)
+print(mul_count)
 print('')
 end_secs = time.time()
 print('--- ' + str(end_secs-start_secs) + ' secs ---')
---
+++ 2017/day23/part1_orig_solution1.py
@@ -0,0 +1,60 @@
+import time
+import sys
+from copy import copy, deepcopy
+start_secs = time.time()
+print('')
+
+def exec_command(s):
+  global registers, iindex, num_mul
+  a = s.split()
+  cmd = a[0]
+  if cmd == 'nop':
+    iindex += 1
+    return  
+  X = a[1]
+  Y = a[2]
+  if cmd == 'jnz':
+    if X.isalpha():
+      X = int(registers[X])
+    else:
+      X = int(X)
+    Y = int(Y)
+    if X != 0:
+      iindex += Y
+      return
+  else:
+    if Y.isalpha():
+      Y = registers[Y]
+    else:
+      Y = int(Y)
+    if cmd == 'set':
+      registers[X] = Y
+    elif cmd == 'sub':
+      registers[X] -= Y
+    elif cmd == 'mul':
+      registers[X] *= Y
+      num_mul += 1
+  iindex += 1
+
+iindex = 0
+num_mul = 0
+registers = dict()
+for c in 'abcdefgh':
+  registers[c] = 0
+
+# read in input file
+l=[]
+my_file = open("inp.txt", "r", encoding='utf-8')
+lines = my_file.readlines()
+for line in lines:
+  l.append(line.strip())
+
+num_commands = len(l)
+while iindex < num_commands:
+  s = l[iindex]
+  exec_command(s)
+
+print(num_mul)
+print('')
+end_secs = time.time()
+print('--- ' + str(end_secs-start_secs) + ' secs ---')
