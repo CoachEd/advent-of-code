26a647d8c0c9456855e032517bac90c59d0bc955
Edwin Torres <CoachEd@gmail.com>
2023-12-12 18:43:54 -0500

    latest

--- 2023/day12/part1.py
+++ 2023/day12/part1.py
@@ -4,12 +4,18 @@ from copy import copy, deepcopy
 import itertools as IT
 
 # read in input file
-my_file = open("inp.txt", "r", encoding='utf-8')
+my_file = open("inp2.txt", "r", encoding='utf-8')
 lines = my_file.readlines()
 l=[None for i in range(len(lines))]
 for i in range(len(lines)):
   l[i] = lines[i].strip()
 
+patterns = []
+sizes = []
+for s in l:
+  arr = s.split()
+  patterns.append(arr[0])
+  sizes.append([ int(c) for c in arr[1].split(',')])
 
 def isMatch(p,s):
   # p is the pattern to match:
@@ -19,6 +25,9 @@ def isMatch(p,s):
   #  no match:     ###.##.....#
   # return True/False
   # TODO: See https://stackoverflow.com/questions/4697882/how-can-i-find-all-matches-to-a-regular-expression-in-python
+  
+  #print((p,s))
+  
   return False
 
 def arrangements(a,n):
@@ -27,59 +36,51 @@ def arrangements(a,n):
   # n is length of string
   # example for [3,2,1],12
   #             .###.##...#.
-  # return array of all possible arrangements (even ones )
+  # return array of all possible arrangements
+
+  # get minimal pattern using sizes
   s = ''
   for i in range(len(a)):
     s = s + '#' * a[i]
     if i < len(a)-1:
       s += '.'
+  plen = len(s)
+  extra = n - plen
+  l = []
+  num_dots = s.count('.')
+  extra += num_dots
+  for s1 in s.split('.'):
+    l.append(s1)
   
-  #  ###.##.#  need to randomly add '.' to make it long enough to meet size n
+  for i in range(extra):
+    l.append('.')
 
-  # 2 balls into 3 slots is like...
-  # 2 balls around 2 sticks (always one fewer stick than slots)
-  # so 4 choose 2
-  # list(IT.combinations(range(4), 2))
-  # [(0, 1), (0, 2), (0, 3), (1, 2), (1, 3), (2, 3)]
-  num_balls = n - len(s)
-  slots = len(a) + 1
-  x = list(IT.combinations(range(num_balls), slots))
-
-  # s is ###.##.# (len 8)
-  # max len 12
-  # bins = 4
-  # num_balls = 4
-  # possibles: (4,0,0,0), (3,1,0,0), (0,0,3,1), (0,0,0,4) HOW?
-  # REGEX? for ###.##.# ? 
-
-  # return array of possibles
-  #(4,0,0,0) (3,1,0,0) (2,2,0,0) 
-  #a2 = list(IT.permutations(range(4), 3))
-  #print(a2)
-# [(0, 1), (0, 2), (0, 3), (1, 2), (1, 3), (2, 3)]
-
-  l = ['###','##','#','.','.','.','.','.','.']
+  
   perms = list(IT.permutations(l))
   permsu = set()
   for p in perms:
     permsu.add(''.join(p))
-
-  for p in permsu:
-    print(p)
-  print()
-  print(len(permsu))
-  return a
+  return list(permsu)
 
 
 # SOLUTION START - start timing
 start_secs = time.time()
 # TODO
 
-
-arrangements([3,2,1],12)
-
+tot_passed = 0
+for i in range(len(patterns)):
+  p = patterns[i]
+  sz = sizes[i]
+  arr = arrangements(sz,len(p))
+  num_passed = 0
+  for s in arr:
+    if isMatch(p,s):
+      num_passed = 0
+  #print(num_passed)
+  tot_passed += num_passed
+  #print(i)
   
-
+#print(tot_passed)
 end_secs = time.time()
 print('--- ' + str(end_secs-start_secs) + ' secs ---')
 # SOLUTION END - stop timing
