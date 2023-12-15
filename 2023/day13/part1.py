4d9c148cadef9cb5e09a22c591f51eca595f0318
Edwin Torres <CoachEd@gmail.com>
2023-12-15 17:10:29 -0500

    part1

--- 2023/day13/part1.py
+++ 2023/day13/part1.py
@@ -30,7 +30,7 @@ def findBiggestFold(m):
         break
     if allIn:
       for y in range(len(folds)):
-        (n,s) = folds[y][x]
+        (n,s1,s2) = folds[y][x]
         if n > maxl:
           maxl = n
           maxi = x
@@ -44,17 +44,28 @@ def getFolds(s):
     s1len = len(s1)
     s2len = len(s2)
 
+    #print((i,s1,s2))
+    shaved_from_left = 0
     if s1len < s2len:
       s2 = s2[0:s1len]
       s1 = s1[::-1]
     elif s2len < s1len:
+      shaved_from_left = s1len-s2len
       s1 = s1[::-1]
       s1 = s1[0:s2len]
-    
+
     s1len = len(s1)
     s2len = len(s2)
-    if s1 == s2:
-      d[i-1] = (len(s1),s1+s2)
+
+    #print((i,s1,s2))
+    #print()
+    n = 0
+    for k in range(len(s1)):
+      if s1[k] == s2[k]:
+        n += 1
+    if n == len(s1):
+      n += shaved_from_left
+      d[i] = (n,s1,s2)
 
   return d
     
@@ -69,7 +80,7 @@ def printOneMatrix(v,i):
   print(s)
 
 # read in input file
-fname = 'inp2.txt'
+fname = 'inp.txt'
 with open(fname, 'r') as file:
   data = file.read()
 lines = data.split('\n')
@@ -95,25 +106,24 @@ for y in range(len(l)):
 #printOneMatrix(v,0)
 
 #print(  getFolds('#####..########')  )
+#sys.exit()
 
 tot = 0
 for m in v:
-  (pos,len1) = findBiggestFold(m)
-  if pos != -1:
-    tot += len1 + 1
+
+  # check vertical
+  (pos1,len1) = findBiggestFold(m)
+  (pos2,len2) = findBiggestFold( rotate_matrix(m) )
+  #print((pos1,len1,'  ', pos2,len2))
+
+  if len1 > len2:
+    tot += len1
   else:
-    # not found, try rotating
-    m2 = rotate_matrix(m) # cclockwise
-    (pos,len1) = findBiggestFold(m2)
-    tot += 100 * (len1+1)
+    tot += 100 * len2
 
 print()
 
 print(tot)
-
-
- 
-
 end_secs = time.time()
 
 print('--- ' + str(end_secs-start_secs) + ' secs ---')
@@ -121,3 +131,5 @@ print('--- ' + str(end_secs-start_secs) + ' secs ---')
 # SOLUTION END - stop timing
 
 # 18180 TOO LOW
+# 30085 TOO LOW
+# 55983 TOO HIGH
