91fdc9c2ba27e75160e0cfa389c8df00515e353a
Edwin Torres <CoachEd@gmail.com>
2023-12-19 14:46:30 -0500

    BROKEN

--- 2023/day17/part1.py
+++ 2023/day17/part1.py
@@ -15,7 +15,7 @@ def print_map(m):
   print(s)
 
 def find_paths(starty, startx, endy, endx, m, visited_nodes, directions):
-  paths = set()
+  visited_nodes.append((startx,starty))
   if starty == endy and startx == endx:
     print(''.join(directions))
     return
@@ -24,6 +24,37 @@ def find_paths(starty, startx, endy, endx, m, visited_nodes, directions):
   (sy,sx) = (starty+1,startx)
   (ey,ex) = (starty,startx+1)
   (wy,wx) = (starty,startx-1)
+
+  ds = ['N','S','E','W']
+  opposite = {}
+  opposite['N'] = 'S'
+  opposite['S'] = 'N'
+  opposite['E'] = 'W'
+  opposite['W'] = 'E'
+  ps = [(ny,nx),(sy,sx),(ey,ex),(wy,wx)]
+  possible_moves = {}
+
+  prev_direction = ''
+  if len(directions) > 0:
+    prev_direction = directions[-1]
+  prev3_directions = ''
+  if len(directions) >= 3:
+    prev3_directions = directions[-3] + directions[-2] + directions[-1]
+  for i in range(len(ds)):
+    (y0,x0) = ps[i]
+    opp = ''
+    if prev_direction in opposite:
+      opp = opposite[prev_direction]
+    if goodp(y0,x0,m) and ds[i] != opp and prev3_directions.count(ds[i]) != 3:
+      possible_moves[ds[i]] = ps[i]
+  
+  for k,v in possible_moves.items():
+    d2 = directions.copy()
+    v2 = visited_nodes.copy()
+    d2.append(k)
+    (y0,x0) = v
+    v2.append((starty,startx))
+    find_paths(y0,x0,endy,endx,m,v2,d2)
 
   
 
