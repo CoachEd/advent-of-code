2b26e6ddb11c5f0ba2b957a2d3cdca0ebe1988e7
Edwin Torres <CoachEd@gmail.com>
2025-01-03 11:40:35 -0500

    latest

--- 2024/daynn/part1.py
+++ 2024/daynn/part1.py
@@ -1,22 +1,32 @@
+#!/usr/bin/env python3
+# pylint: disable=W0611,C0103,W0511
+"""
+This module contains functions for part 1
+"""
+
 import time
 import sys
 from copy import copy, deepcopy
 
 # read in input file
 fname = 'inp.txt'
-with open(fname, 'r') as file:
-  data = file.read()
+with open(fname, 'r', encoding="utf-8") as file:
+    data = file.read()
 lines = data.split('\n')
 l=[None for i in range(len(lines))]
-for i in range(len(lines)):
-  l[i] = lines[i].strip()
-#print(l)
+for i, line in enumerate(lines):
+    l[i] = line.strip()
+print(l)
 
 # SOLUTION START - start timing
 start_secs = time.time()
 
 
 # TODO
+
+
+
+
 
 
 end_secs = time.time()
